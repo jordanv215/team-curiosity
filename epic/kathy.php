37795e4815d5a3a34c80a6302e45a4662f0e599e
Kai Garrott <garrottkai@gmail.com>
2016-04-27 16:56:56 -0600

    Add another persona

---
+++ epic/kathy.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Persona: Kathy</title>
+	</head>
+	<body>
+		<header>
+			<h1>Kathy</h1>
+		</header>
+		<main>
+			<p>Kathy Sorenson: 45 years old, works in the office of a local construction company. She has two children in middle school. Kathy is normally very busy, but she enjoys walking her dogs and on the weekends she likes to watch shows on Netflix.</p>
+			<h4>Use Case</h4>
+			<p></p>
+		</main>
+	</body>
+</html>
