6432ecdf0666959873522573fd408f23e68bcaaf
Jordan Vinson <jvinson3@cnm.edu>
2016-04-27 16:38:42 -0600

    Added 2 more user stories

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -13,7 +13,7 @@
 				<li>As a user who just finished watching Cosmos, I'm interested in the <em>Curiosity</em> project, but I find that NASA's website is cumbersome and hard to navigate.</li>
 				<li>I am a 6th grade student doing a Science Fair project on Planetary weather, and want to learn more about Mars</li>
 				<li>I am a billionaire with huge ties in the private space industry, and I'm looking to hire a group of web developers for creation of an app.</li>
-				<li>I'm a 25 year old game developer creating a FPS on Mars, and need to know the terrain and weather</li>
+				<li>I'm a 25 year old game developer creating a FPS set on Mars, and need to know the terrain and weather</li>
 			</ul>
 		</main>
 	</body>
