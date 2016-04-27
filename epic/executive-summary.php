510a6c83c0b5f963df9da815edb12ded7d9c3e54
Kai Garrott <garrottkai@gmail.com>
2016-04-27 16:05:38 -0600

    Added separate files for epic

---
+++ epic/executive-summary.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Executive Summary</title>
+	</head>
+	<body>
+		<header>
+			<h1>Executive Summary</h1>
+		</header>
+		<main>
+			<p>The goal of Team Mars is to create an app/website that allows users to view realtime updates, photos, current weather, and information about the <em>Curiosity</em> Rover's mission on Mars. We aim to present this data in a clean, simple format to improve the accessibility of NASA's raw data to the public.</p><br>
+		</main>
+	</body>
+</html>
