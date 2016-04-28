4af09d0e08dba45b4138cac9157ae2049c75fb3e
Anthony Williams <pull.rebase>
2016-04-28 06:20:23 -0600

    Added User persona frances

---
+++ epic/frances.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="utf-8" />
+	<title>Persona: Frances</>
+	</head>
+	<body>
+		<header>
+			<h1>Frances</h1>
+		</header>
+		<main>
+			<p>Frances Deleon: 58 years old, an expert searcher.  She recently took over the lead on organizing her 40 year class reunion.  The theme of the reunion will be all about mars, since she has always been interested in outer space but never had the time to continue her education due to being a stay at home mother.  She likes to keep up with Nasa launch missions and never misses a Rocket launch always glued to her television dreaming of the outer space.
+ After the launch was over she began searching Google, her favorite search engine looking for images from mars she could use for her class reunion theme and she came across our site curiosity rover, she noticed the fresh look and feel of the site.
