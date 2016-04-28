a37f473b080a1171bba2ac6df686b36405718f4a
Kai Garrott <garrottkai@gmail.com>
2016-04-28 08:34:53 -0600

    Minor edits

--- epic/frances.php
+++ epic/frances.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta charset="utf-8" />
-	<title>Persona: Frances</>
+	<meta charset="UTF-8" />
+	<title>Persona: Frances</title>
 	</head>
 	<body>
 		<header>
 			<h1>Frances</h1>
 		</header>
 		<main>
-			<p>Frances Deleon: 58 years old, an expert searcher.  She recently took over the lead on organizing her 40 year class reunion.  The theme of the reunion will be all about mars, since she has always been interested in outer space but never had the time to continue her education due to being a stay at home mother.  She likes to keep up with Nasa launch missions and never misses a Rocket launch always glued to her television dreaming of the outer space.
- After the launch was over she began searching Google, her favorite search engine looking for images from mars she could use for her class reunion theme and she came across our site curiosity rover, she noticed the fresh look and feel of the site.
+			<p>Frances Deleon: 58 years old, an expert searcher.  She recently took over the lead in organizing her 40 year class reunion.  The theme of the reunion will be all about Mars, since she has always been interested in space but has never had the time to continue her education, due to being a stay-at-home mother.  She likes to keep up with NASA missions and never misses a rocket launch, glued to her television dreaming of outer space.</p>
+ <h4>Use Case</h4><p>After the latest launch was over, she began searching Google, her favorite search engine, looking for images from Mars that she could use for her class reunion theme. When she came across our site about the <em>Curiosity</em> rover, she noticed the fresh look and feel of the site.</p>
+		</main
+	</body>
+</html>
