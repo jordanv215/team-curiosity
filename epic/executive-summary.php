f7fe4f3e617cff1740be86e6a3960dbb88e0b7b4
Ellen Liu <huailun@hotmail.com>
2016-04-28 09:27:39 -0600

    person-lalit

--- epic/executive-summary.php
+++ epic/executive-summary.php
@@ -9,6 +9,9 @@
 			<h1>Executive Summary</h1>
 		</header>
 		<main>
+			<p>
+				
+			</p>
 			<p>The goal of Team Mars is to create an app/website that allows users to view realtime updates, photos, current weather, and information about the <em>Curiosity</em> Rover's mission on Mars. We aim to present this data in a clean, simple format to improve the accessibility of NASA's raw data to the public.</p><br>
 		</main>
 	</body>
---
+++ epic/lalit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Persona: Lalit</title>
+	</head>
+	<body>
+		<header>
+			<h1>Lalit Datta</h1>
+		</header>
+		<main>
+			<p>Lalit Datta: 25 years old, works as a game designer. He just has promoted to a project manager of a new game design. It is going to be a top relating to "Mars". He needs to create a concept for the game, and is looking for information of the geography and weather on Mars in order to make game as realistic as possible.
+			<h4>Use Case</h4>
+			 <p></p>
+		</main>
+		
+		</body>
+</html>
