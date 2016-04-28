6d0b843af7ac0f4d9a23949cde3e025f1b308f81
Jordan Vinson <jvinson3@cnm.edu>
2016-04-28 15:33:40 -0600

    Added 2 more user stories

--- epic/frances.php
+++ epic/frances.php
@@ -10,7 +10,8 @@
 		</header>
 		<main>
 			<p>Frances Deleon: 58 years old, an expert searcher.  She recently took over the lead in organizing her 40 year class reunion.  The theme of the reunion will be all about Mars, since she has always been interested in space but has never had the time to continue her education, due to being a stay-at-home mother.  She likes to keep up with NASA missions and never misses a rocket launch, glued to her television dreaming of outer space.</p>
- <h4>Use Case</h4><p>After the latest launch was over, she began searching Google, her favorite search engine, looking for images from Mars that she could use for her class reunion theme. When she came across our site about the <em>Curiosity</em> rover, she noticed the fresh look and feel of the site.</p>
+ 			<h4>Use Case</h4>
+			<p>After the latest launch was over, she began searching Google, her favorite search engine, looking for images from Mars that she could use for her class reunion theme. When she came across our site about the <em>Curiosity</em> rover, she noticed the fresh look and feel of the site.</p>
 		</main
 	</body>
 </html>
