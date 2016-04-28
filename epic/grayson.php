5995586195abe71dc78c5d991e1fa229efdea668
Jordan Vinson <jvinson3@cnm.edu>
2016-04-28 16:52:52 -0600

    Deleted Lalit and Frances. Added equipment for Isaac

--- epic/grayson.php
+++ epic/grayson.php
@@ -9,7 +9,7 @@
 			<h1>Grayson</h1>
 		</header>
 		<main>
-			<p>Grayson Jaramillo: 19 years old, currently attending CNM and working at a painting company. In 10th grade, Grayson began watching <em>Cosmos</em> in his free time. After watching the entire series, he found that the episode about planets was specifically interesting to him, and he especially wanted to learn more about Mars.</p>
+			<p>Grayson Jaramillo: 19 years old, currently attending CNM and working at a painting company. In 10th grade, Grayson began watching <em>Cosmos</em> in his free time. After watching the entire series, he found that the episode about planets was specifically interesting to him, and he especially wanted to learn more about Mars. Using his NexGen Academy provided Dell laptop, he found the Deep Dive <em>Curiosity</em> site.</p>
 			<h4>Use Case</h4>
 			<p>Throughout the next couple years while he was attending high school, he used the website on his NexGen High School-provided Dell laptop to learn more and more about Mars. Currently, he is planning to attend a high-level university in order to become a major in astrophysics.</p>
 		</main>
--- epic/isaac.php
+++ epic/isaac.php
@@ -9,9 +9,9 @@
 			<h1>Isaac Chen</h1>
 		</header>
 		<main>
-			<p>Isaac Chen: 9 years old, currently a third-grader in elementary school. He has been interested in exploring outer space since a very young age. Isaac is not a regular computer user, occasionally consulting the Internet to help with school projects.</p>
+			<p>Isaac Chen: 9 years old, currently a third-grader in elementary school. He has been interested in exploring outer space since a very young age. Isaac is not a regular computer user, occasionally consulting the Internet using Safari on his Apple Ipad 2 to help with school projects.</p>
 			<h4>Use Case</h4>
-			<p>After his school announced its upcoming science fair, Isaac began seriously thinking about doing a project about Mars. He did a Google search on Safari using his Ipad 2 and found many results; however, the websites he found were too complicated for him to use effectively. He asked his science teacher to help him out. During class, they worked together and finally discovered the "Team Curiosity" website. They found that it was very accessible, and were able to easily get the exact information that Isaac needed for his project, such as photos and climate information from Nasa's databases. The interface of the application was simple and intuitive to both of them, even with Isaac's limited computer experience. They also enjoyed the realtime updates, which kept Isaac coming back to the site even after the science fair was over.</p>
+			<p>After his school announced its upcoming science fair, Isaac began seriously thinking about doing a project about Mars. He did a Google search and found many results; however, the websites he found were too complicated for him to use effectively. He asked his science teacher to help him out. During class, they worked together and finally discovered the "Team Curiosity" website. They found that it was very accessible, and were able to easily get the exact information that Isaac needed for his project, such as photos and climate information from Nasa's databases. The interface of the application was simple and intuitive to both of them, even with Isaac's limited computer experience. They also enjoyed the realtime updates, which kept Isaac coming back to the site even after the science fair was over.</p>
 		</main>
 	</body>
 </html>
