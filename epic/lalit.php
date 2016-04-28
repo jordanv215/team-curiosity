0e3b77096ac6b5473f3bb7fc3db06a62371c026c
Jordan Vinson <jvinson3@cnm.edu>
2016-04-28 15:45:17 -0600

    Fixed the spelling of Data  in Lalit's profile

--- epic/lalit.php
+++ epic/lalit.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<header>
-			<h1>Lalit Datta</h1>
+			<h1>Lalit Data</h1>
 		</header>
 		<main>
 			<p>Lalit Datta: 35 year old game designer working at a very well known game development company. Recently, Lalit was  promoted to project manager on a FPS/RPG set on the surface of Mars. He needs to create a concept for the game, and is looking for information on the geography and weather on Mars in order to make the game as realistic as possible.
