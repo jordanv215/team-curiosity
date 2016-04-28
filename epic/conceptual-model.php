6f899c2245f4bb7cb7c2a755728570c5e824dee2
Kai Garrott <garrottkai@gmail.com>
2016-04-28 15:48:18 -0600

    Add conceptual model

---
+++ epic/conceptual-model.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<title>Conceptual Model</title>
+	</head>
+	<body>
+		<header>
+			<h1>Conceptual Model</h1>
+		</header>
+		<main>
+			<p>This app will initially load a splash page containing a highlight feed of the most recent images and article headlines. A bar across the page will display essential weather info, clickable to show the full set of current data. The main page will also contain a simplified menu offering direct access to images and articles.<br>
+			The images page will contain an option to view the featured images. It will be searchable by sol, Earth date, and camera. The articles page will be searchable by source and keywords.</p>
+		</main>
+	</body>
+</html>
