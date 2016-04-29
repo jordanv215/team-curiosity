ee3823d01adaf86d8c421b29898f6c8b099c66df
Kai Garrott <garrottkai@gmail.com>
2016-04-29 08:26:17 -0600

    Add more user stories

--- epic/system-goals.php
+++ epic/system-goals.php
@@ -14,6 +14,10 @@
 				<li>To provide a pleasant user experience to people interested in the <em>Curiosity</em> project.</li>
 				<li>To streamline access to project data and eliminate the poor UX provided by the cumbersome NASA website</li>
 				<li>To make all of this information accessible in two clicks or less from the main page.</li>
+				<li>To offer a place for enthusiasts to keep up with events on Mars</li>
+				<li>To interface with NASA's live updates via the Mars Imaging API</li>
+				<li>To pull weather data from a NASA site and display it clearly</li>
+				<li>To show a feed of relevant scientific news of interest to casual readers</li>
 			</ul>
 		</main>
 	</body>
