71a693dad94cad29446b682a8920f396e8c93ca7
Jordan Vinson <jvinson3@cnm.edu>
2016-04-28 18:50:21 -0600

    Deleted Lalit and Frances. Added equipment for Isaac

--- epic/user-stories.php
+++ epic/user-stories.php
@@ -12,6 +12,8 @@
 			<ul>
 				<li>As a user who just finished watching Cosmos, I'm interested in the <em>Curiosity</em> project, but I find that NASA's website is cumbersome and hard to navigate.</li>
 				<li>I am a student doing a Science Fair project on Planetary weather, and want to learn more about Mars</li>
+				<li>I am creating a blog about Mars, and need images from an easy to access database on a daily basis</li>
+				<li>I'm very interested in NASA's projects, and want an easy way to get news about <em>Curiosity</em></li>
 			</ul>
 		</main>
 	</body>
