c41d42423854e1ce98195b078915008e2812a85f
Jordan Vinson <jvinson3@cnm.edu>
2016-04-29 08:38:37 -0600

    Finished adding main entities to Conceptual Model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -11,6 +11,11 @@
 		<main>
 			<p>This app will initially load a splash page containing a highlight feed of the most recent images and article headlines. A bar across the page will display essential weather info, clickable to show the full set of current data. The main page will also contain a simplified menu offering direct access to images and articles.<br>
 			The images page will contain an option to view the featured images. It will be searchable by sol, Earth date, and camera. The articles page will be searchable by source and keywords.</p>
+			<h1>User</h1>
+			<ul>
+				<li>social media login</li>
+				<li>userID</li>
+			</ul>
 			<h1>Images</h1>
 			<ul>
 				<li>title of image</li>
@@ -19,7 +24,12 @@
 				<li>camera used</li>
 				<li>Earth date picture was taken on</li>
 				<li>sol (Mars day) date picture was taken on</li>
-
+			</ul>
+			<h1>News</h1>
+			<ul>
+				<li>article ID</li>
+				<li>source</li>
+				<li>article date</li>
 			</ul>
 		</main>
 	</body>
