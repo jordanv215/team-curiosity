bc7a78b48f8b76c3986de8c15900431a62f3196d
Jordan Vinson <jvinson3@cnm.edu>
2016-04-29 09:05:42 -0600

    Added relations to conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -11,12 +11,12 @@
 		<main>
 			<p>This app will initially load a splash page containing a highlight feed of the most recent images and article headlines. A bar across the page will display essential weather info, clickable to show the full set of current data. The main page will also contain a simplified menu offering direct access to images and articles.<br>
 			The images page will contain an option to view the featured images. It will be searchable by sol, Earth date, and camera. The articles page will be searchable by source and keywords.</p>
-			<h1>User</h1>
+			<h2>User</h2>
 			<ul>
 				<li>social media login</li>
 				<li>userID</li>
 			</ul>
-			<h1>Images</h1>
+			<h2>Images</h2>
 			<ul>
 				<li>title of image</li>
 				<li>Image ID</li>
@@ -25,30 +25,38 @@
 				<li>Earth date picture was taken on</li>
 				<li>sol (Mars day) date picture was taken on</li>
 			</ul>
-			<h1>News</h1>
+			<h2>News</h2>
 			<ul>
 				<li>article ID</li>
 				<li>source</li>
 				<li>article date</li>
 			</ul>
 			<h1>Relations</h1>
-			<h3>User</h3>
+			<h2>User</h2>
 			<ul>
-				<li>user may favorite image</li>
-				<li>user may favorite article</li>
-				<li>user may comment on article</li>
-				<li>user may comment on image</li>
+				<li>user may favorite image - leads to favoriteImage</li>
+				<li>user may favorite article - leads to favoriteArticle</li>
+				<li>user may comment on article - leads to commentArticle</li>
+				<li>user may comment on image - leads to commentImage</li>
 			</ul>
-			<h3>Image</h3>
+			<h2>Image</h2>
 			<ul>
-				<li>image may be favorited</li>
-				<li>image may be commented on</li>
+				<li>image may be favorited - leads to favoriteImage</li>
+				<li>image may be commented on - leads to commentImage</li>
 			</ul>
-			<h3>News</h3>
+			<h2>News</h2>
 			<ul>
-				<li>article may be favorited</li>
-				<li>article may be commented on</li>
+				<li>article may be favorited - leads to favoriteArticle</li>
+				<li>article may be commented on - leads to commentArticle</li>
 			</ul>
+			<h2>Weak Entities</h2>
+			<h3>commentImage</h3>
+			<ul>
+					<li>imageId</li>
+					<li>userId</li>
+					<li>dateTime</li>
+			</ul>
+
 		</main>
 	</body>
 </html>
