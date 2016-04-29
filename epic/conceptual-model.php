48b2e78263e5a4136eba69e003ab4f24085292c2
Jordan Vinson <jvinson3@cnm.edu>
2016-04-29 08:30:00 -0600

    Began working on conceptual model

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -11,6 +11,16 @@
 		<main>
 			<p>This app will initially load a splash page containing a highlight feed of the most recent images and article headlines. A bar across the page will display essential weather info, clickable to show the full set of current data. The main page will also contain a simplified menu offering direct access to images and articles.<br>
 			The images page will contain an option to view the featured images. It will be searchable by sol, Earth date, and camera. The articles page will be searchable by source and keywords.</p>
+			<h1>Images</h1>
+			<ul>
+				<li>title of image</li>
+				<li>Image ID</li>
+				<li>image description</li>
+				<li>camera used</li>
+				<li>Earth date picture was taken on</li>
+				<li>sol (Mars day) date picture was taken on</li>
+
+			</ul>
 		</main>
 	</body>
 </html>
