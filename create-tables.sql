09cc9869133dcc60166d5d998380e2b062102ad9
Anthony Williams <pull.rebase>
2016-05-04 10:31:33 -0600

    Edited SQL database https://app.asana.com/0/117435667927136/119450495650305

--- create-tables.sql
+++ create-tables.sql
@@ -1,28 +1,28 @@
-DROP TABLE IF EXISTS loginSource;
-DROP TABLE IF EXISTS user;
-DROP TABLE IF EXISTS image;
-DROP TABLE IF EXISTS newsArticle;
-DROP TABLE IF EXISTS commentImage;
-DROP TABLE IF EXISTS favoriteImage;
-DROP TABLE IF EXISTS commentNewsArticle;
 DROP TABLE IF EXISTS favoriteNewsArticle;
+DROP TABLE IF EXISTS commentNewsArticle;
+DROP TABLE IF EXISTS favoriteImage;
+DROP TABLE IF EXISTS commentImage;
+DROP TABLE IF EXISTS newsArticle;
+DROP TABLE IF EXISTS image;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS loginSource;
 
 CREATE TABLE loginSource (
-	loginSourceId VARCHAR(256) NOT NULL,
+	loginSourceId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	loginSourceApiKey VARCHAR(256) NOT NULL,
 	loginSourceProvider VARCHAR(128) NOT NULL,
-	INDEX(loginSourceProvider),
 	PRIMARY KEY(loginSourceId)
 );
 
 CREATE TABLE user (
 	userId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	userEmail VARCHAR(128) NOT NULL,
-	userLoginId VARCHAR(256) NOT NULL,
+	userLoginId INT UNSIGNED NOT NULL,
 	userName VARCHAR(128) NOT NULL,
 	INDEX(userName),
 	UNIQUE(userEmail),
-	UNIQUE(userLoginId),
+	INDEX (userLoginId),
+	FOREIGN KEY (userLoginId) REFERENCES loginSource(loginSourceId),
 	PRIMARY KEY(userId)
 );
 
@@ -31,11 +31,11 @@ CREATE TABLE image (
 	imageCamera VARCHAR(64) NOT NULL,
 	imageDescription VARCHAR(5000),
 	imageEarthDate DATETIME NOT NULL,
-	imagePath VARCHAR(512) NOT NULL,
-	imageSol TINYINT UNSIGNED NOT NULL,
+	imagePath VARCHAR(256) NOT NULL,
+	imageSol SMALLINT UNSIGNED NOT NULL,
 	imageTitle VARCHAR(128) NOT NULL,
-	imageType VARCHAR(32) NOT NULL,
-	imageUrl VARCHAR(512) NOT NULL,
+	imageType VARCHAR(10) NOT NULL,
+	imageUrl VARCHAR(256) NOT NULL,
 	INDEX(imageCamera),
 	INDEX(imageEarthDate),
 	INDEX(imageSol),
@@ -47,8 +47,8 @@ CREATE TABLE image (
 CREATE TABLE newsArticle (
 	newsArticleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	newsArticleDate DATETIME NOT NULL,
-	newsArticleSynopsis CHAR(256) NOT NULL,
-	newsArticleUrl VARCHAR(512) NOT NULL,
+	newsArticleSynopsis VARCHAR(256) NOT NULL,
+	newsArticleUrl VARCHAR(256) NOT NULL,
 	INDEX(newsArticleDate),
 	INDEX(newsArticleSynopsis),
 	UNIQUE(newsArticleUrl),
@@ -58,10 +58,9 @@ CREATE TABLE newsArticle (
 CREATE TABLE commentImage (
 	commentImageId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	commentImageContent VARCHAR(1024) NOT NULL,
-	commentImageDateTime DATETIME NOT NULL,
+	commentImageDateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	commentImageImageId INT UNSIGNED NOT NULL,
 	commentImageUserId INT UNSIGNED NOT NULL,
-	INDEX(commentImageDateTime),
 	INDEX(commentImageImageId),
 	INDEX(commentImageUserId),
 	FOREIGN KEY(commentImageImageId) REFERENCES image(imageId),
@@ -72,7 +71,9 @@ CREATE TABLE commentImage (
 CREATE TABLE favoriteImage (
 	favoriteImageImageId INT UNSIGNED NOT NULL,
 	favoriteImageUserId INT UNSIGNED NOT NULL,
-	favoriteImageDateTime DATETIME NOT NULL,
+	favoriteImageDateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	INDEX(favoriteImageImageId),
+	INDEX(favoriteImageUserId),
 	FOREIGN KEY(favoriteImageImageId) REFERENCES image(imageId),
 	FOREIGN KEY(favoriteImageUserId) REFERENCES user(userId),
 	PRIMARY KEY(favoriteImageImageId, favoriteImageUserId)
@@ -81,10 +82,9 @@ CREATE TABLE favoriteImage (
 CREATE TABLE commentNewsArticle (
 	commentNewsArticleId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	commentNewsArticleContent VARCHAR(1024) NOT NULL,
-	commentNewsArticleDateTime DATETIME NOT NULL,
+	commentNewsArticleDateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	commentNewsArticleNewsArticleId INT UNSIGNED NOT NULL,
 	commentNewsArticleUserId INT UNSIGNED NOT NULL,
-	INDEX(commentNewsArticleDateTime),
 	INDEX(commentNewsArticleNewsArticleId),
 	INDEX(commentNewsArticleUserId),
 	FOREIGN KEY(commentNewsArticleNewsArticleId) REFERENCES newsArticle(newsArticleId),
@@ -95,7 +95,9 @@ CREATE TABLE commentNewsArticle (
 CREATE TABLE favoriteNewsArticle (
 	favoriteNewsArticleNewsArticleId INT UNSIGNED NOT NULL,
 	favoriteNewsArticleUserId INT UNSIGNED NOT NULL,
-	favoriteNewsArticleDateTime DATETIME NOT NULL,
+	favoriteNewsArticleDateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	INDEX(favoriteNewsArticleNewsArticleId),
+	INDEX(favoriteNewsArticleUserId),
 	FOREIGN KEY(favoriteNewsArticleNewsArticleId) REFERENCES newsArticle(newsArticleId),
 	FOREIGN KEY(favoriteNewsArticleUserId) REFERENCES user(userId),
 	PRIMARY KEY(favoriteNewsArticleNewsArticleId, favoriteNewsArticleUserId)
