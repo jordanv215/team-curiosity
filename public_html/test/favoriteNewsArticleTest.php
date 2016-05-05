e51dec74a147ff61f6364258ad2cd5ecb337693d
Ellen Liu <huailun@hotmail.com>
2016-05-05 13:44:49 -0600

    added TeamCuriosityTest

---
+++ public_html/test/favoriteNewsArticleTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Edu\Cnm\TeamCuriosity\Test;
+
+use Edu\Cnm\TeamCuriosity\{user, newsArticle, favoriteNewsArticle};
+
+// grab the project test parameters
+require_once ("TeamCuriosityTest.php");
+
+//grab the class under scrutiny
+require_once(dirname(__DIR__) . "/php/classes/autoload.php");
+
+/**
+ * FULL PHPUnit test for the favoriteNewsArticle class
+ *
+ * This is a complete PHPUnit test of the favoriteNewsArticle class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ **/
+class favoriteNewsArticleTest extends TeamCuriosityTest {
+	/**
+	 * content of the favoriteNewsArticle
+	 * @var 
+	 */
+}
