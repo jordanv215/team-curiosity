88b3e7c16dad673ffa172204964d81ac0a37505c
Jordan Vinson <jvinson3@cnm.edu>
2016-05-05 16:14:15 -0600

    Began coding test for User https://app.asana.com/0/117435667927136/119450495650305

--- public_html/test/usertest.php
+++ public_html/test/usertest.php
@@ -18,17 +18,51 @@ require_once (dirname(__DIR__)) . "/php/classes/Autoload.php");
  * @see user
  * @author Jordan Vinson <jvinson3@cnm.edu>
  */
-class usertest extends TeamCuriosityTest {
+class UserTest extends TeamCuriosityTest {
 	/**
-	 * content of this tweet
+	 * user
 	 * @var string $VALID_USER
 	 */
-	protected $VALID_USER = "PHPUnit test passing";
+	protected $VALID_EMAIL = "PHPUnit test passing";
+	protected $VALID_EMAIL2 = "PHPUnit test passing again";
 	/**
-	 * content of the updated Tweet
+	 * user
 	 * @var string $VALID_USER2
 	 **/
-	protected $VALID_USER2= "PHPUnit test passing";
+	protected $VALID_USERNAME = "PHPUnit test still passing";
+
+	protected $LoginSource = null;
+
+	//create dependant objects before running each test
+
+	public final function setup() {
+		//run the default setup() method first
+		parent::setup();
+
+		//create and insert a LoginSource to own the test user
+		$this->LoginSource = new LoginSource(null, "@phpunit", "@testphpunit.de", "12125551212");
+		$this->loginSource->insert($this->getPDO());
+
+		$this->VALID_EMAIL = new \User();
+
+	}
+
+	//test inserting a valid email and verify that it matches the mySQL data
+	public function testInsertValidUser() {
+		//count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("email");
+
+		//create a new user and insert it into mySQL
+		$user = new User(null, $this->user->getUserId(), $this->VALID_EMAIL, $this->VALID_USERNAME);
+		$user->insert($this->getPDO());
+
+		//grab the data from mySQL and enforce the fields to match our expectations
+		$pdoUser = User::getUserbyUserID($this->getPDO(), $user ->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+		$this->assertEquals($pdoUser->getEmail)
+	}
+
+
 
 }
 
