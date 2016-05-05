5fb5cb97de4ce62526fd89befa9b7594d26141dc
Kai Garrott <garrottkai@gmail.com>
2016-05-05 16:46:36 -0600

    update LoginSourceTest https://app.asana.com/0/117435667927136/119450510065334

--- public_html/test/LoginSourceTest.php
+++ public_html/test/LoginSourceTest.php
@@ -20,14 +20,19 @@ require_once(dirname(_DIR_) . "/php/classes/loginSource.php");
 class LoginSourceTest extends TeamCuriosityTest {
 	/**
 	 * api key of the login source
-	 * @var string $VALID_LOGINSOURCEID
+	 * @var string $VALID_LOGIN_SOURCE_API_KEY
 	 */
-	protected $VALID_LOGIN_SOURCE_API_KEY;
+	protected $VALID_LOGIN_SOURCE_API_KEY = "abc123def456";
+	/**
+	 * another api key for login source
+	 * @var string $VALID_LOGIN_SOURCE_API_KEY2
+	 */
+	protected $VALID_LOGIN_SOURCE_API_KEY2 = "xyz789uvw000";
 	/**
 	 * social media login provider
 	 * @var string $VALID_LOGIN_SOURCE_PROVIDER
 	 */
-	protected $VALID_LOGIN_SOURCE_PROVIDER;
+	protected $VALID_LOGIN_SOURCE_PROVIDER = "congratulations";
 	/**
 	 * setup method
 	 */
@@ -47,6 +52,42 @@ class LoginSourceTest extends TeamCuriosityTest {
 		$pdoLoginSource = LoginSource::getLoginSourceByLoginSourceId($this->getPDO(), $LoginSource->getLoginSourceId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("LoginSource"));
 		$this->assertEquals($pdoLoginSource->getLoginSourceId(), $this->LoginSource->getLoginSourceId());
-		$this->assertEquals($pdoLoginSource->)
+		$this->assertEquals($pdoLoginSource->getLoginSourceApiKey(), $this->LoginSource->getLoginSourceApiKey());
+		$this->assertEquals($pdoLoginSource->getLoginSourceProvider(), $this->LoginSource->getLoginSourceProvider());
 	}
+
+	/**
+	 *  test inserting a login source with a primary key that already exists
+	 *
+	 * @expectedException PDOException
+	 */
+	public function testInsertInvalidLoginSource() {
+		// create a login source with a non null id - it should fail
+		$LoginSource = new LoginSource(TeamCuriosityTest::INVALID_KEY, $this->VALID_LOGIN_SOURCE_API_KEY, $this->VALID_LOGIN_SOURCE_PROVIDER);
+		$LoginSource->insert($this->getPDO());
+	}
+	
+	/**
+	 * test inserting a login source, editing it, then updating it
+	 */
+	public function testUpdateValidLoginSource() {
+		// count the number of rows
+		$numRows = $this->getConnection()->getRowCount("LoginSource");
+		
+		//create a new login source and insert it into table
+		$LoginSource = new LoginSource(null, $this->LoginSource->getLoginSourceApiKey(), $this->VALID_LOGIN_SOURCE_API_KEY, $this->VALID_LOGIN_SOURCE_PROVIDER);
+		$LoginSource->insert($this->getPDO());
+		
+		// edit the login source and update it in table
+		$LoginSource->setLoginSourceApiKey($this->VALID_LOGIN_SOURCE_API_KEY2);
+		$LoginSource->update($this->getPDO());
+		
+		// grab the table data and enforce that fields match expectations
+		$pdoLoginSource = LoginSource::getLoginSourceByLoginSourceId($this->getPDO(), $LoginSource->getLoginSourceId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("LoginSource"));
+		$this->assertEquals($pdoLoginSource->getLoginSourceApiKey(), $this->LoginSource->getLoginSourceApiKey());
+		$this->assertEquals($pdoLoginSource->getLoginSourceProvider(), $this->LoginSource->getLoginSourceProvider());
+	}
+	
+	
 }
