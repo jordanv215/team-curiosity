42a8f5633ee48c6093c3c8ef981cd86afffd84b2
Ellen Liu <huailun@hotmail.com>
2016-05-06 09:32:08 -0600

    added image.php https://app.asana.com/0/117435671931129/119450510065338

--- public_html/php/classes/Image.php
+++ public_html/php/classes/Image.php
@@ -8,6 +8,7 @@ require_once("Autoload.php");
  **/
 class Image implements \JsonSerializable {
 	use ValidateDate;
+
 	/**
 	 * constructor for this image
 	 *
@@ -23,7 +24,7 @@ class Image implements \JsonSerializable {
 	 * @throws \InvalidArgumentException if data types are not valid
 	 * @throws \RangeException if data values are out of bounds (e.g. strings too long, negarive intergers)
 	 * @throws \TypeError if data types violate type hints
-	 * @throws  \Exception if some other exception occurs
+	 * @throw  \Exception if some other exception occurs
 	 **/
 	public function __construct(int $newImageId = null, string $newImageCamera, string $newImageDescription, $newImageEarthDate, string $newImagePath, int $newImageSol, string $newImageTitle, string $newImageType, string $newImageUrl) {
 		try {
@@ -38,15 +39,87 @@ class Image implements \JsonSerializable {
 			$this->setImageUrl($newImageUrl);
 		} catch(\InvalidArgumentException $invalidArgument) {
 			// rethrow the exception to the caller
-				throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
-		}	catch(\RangeException $range) {
-				//rethrow the exception to the caller
-				throw(new \RangeException($range->getMessage(), 0, $range));
-		}	catch(\TypeError $typeError) {
-				// rethrow the exception to the caller
-				throw(new \TypeError($typeError->getMessage(), 0, $typeError));
-		}	catch(\Exception $exception) {
-				// rethrow the exception to the caller
-				throw(new \Exception($exception->getMessage(), 0, $exception));
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			//rethrow the exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			// rethrow the exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			// rethrow the exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
 		}
 	}
+
+	/**
+	 * accessor method for image id
+	 *
+	 * @return int | null value of image id
+	 **/
+	public function getImageId() {
+		return ($this->imageId);
+	}
+
+	/**
+	 * mutator moethod for image id
+	 *
+	 * @param int | null $newImageId new value of image id
+	 * @throws \RangeException if $newImageId is not positive
+	 * @throws \TypeError if $newImageId is not an integer
+	 **/
+	public function setImageId(int $newImageId = null) {
+		// base case: if the image id is null, this is a new image without a mySQL assigned id(yet)
+		if($newImageId === null) {
+			$this->imageId = null;
+			return;
+		}
+
+		//verify the image id is positive
+		if($newImageId <= 0) {
+			throw(new \RangeException("image id is not positive"));
+		}
+
+		//convert and  store the image id
+		$this->imageId = $newImageId;
+	}
+
+	/** accessor method for image camera
+	 *
+	 * @return string value of image camera
+	 **/
+	public function getImageCamera() {
+		return ($this->imageCamera);
+	}
+
+	/** matator method for image camera
+	 * @param string $newImageCamera new value of image camera
+	 * @throws \InvalidArgumentException if $newImageCamera is not a string of insecure
+	 * @throws \RangeException is $newImageCamera is > 64 characters
+	 * @throws \TypeError is $newImageCamera is not a string
+	 **/
+	public function setImageCamera() {
+		// verify the image camera is secure
+		$newImageCamera = trim($newImageCamera);
+		$newImageCamera = filter_var($newImageCamera, FILTER_SANITIZE_STRING);
+		if(empty($newImageCamera) === true) {
+			throw(new \InvalidArgumentException("image camera is empty or insecure"));
+		}
+
+		// verify the image camera will fit in the database
+		if(strlen($newImageCamera) > 64) {
+			throw(new \RangeException("image camera too large"))
+		}
+
+		//store the image camera
+		$this->imageCamera = $newImageCamera;
+	}
+
+	/** accessor method for imageDescription
+	 *
+	 * @return string value of imageDescription
+	 **/
+	public function getImageDescription() {
+		return($this->getImageDescription);
+	}
+}
--- public_html/test/favoriteNewsArticleTest.php
+++ public_html/test/favoriteNewsArticleTest.php
@@ -17,7 +17,53 @@ require_once(dirname(__DIR__) . "/php/classes/autoload.php");
  **/
 class FavoriteNewsArticleTest extends TeamCuriosityTest {
 	/**
-	 * timestamp of the FavoriteNewsArticle;
+	 * timestamp of the FavoriteNewsArticle; this starts as null and is assigned later
+	 * @var DateTime $VALID_FAVORITENEWSARTICLEDATETIME
+	 **/
+	protected $VALID_FAVORITENEWSARTICLEDATETIME = null;
+	/**
+	 * user that favorites the FavoriteNewsArticle; this is for foreign key relations
+	 * @var User $user
+	 *
+	 * NewsArticle that is favorited; this is for foreign key relations
+	 * @var NewsArticle $newsArticle
+	 **/
+	protected $user = null;
+	protected $newsArticle = null;
+
+	/**
+	 *create dependent objects before running each test
+	 **/
+	public final function setUp() {
+		// run the default setUp() method first
+		parent::setUp();
+
+		// create and insert a Profile to own the test FavoriteNewsArticle
+		$this->user = new User(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->user->insert($this->getPDO());
+
+		// create and insert a NewsArticle to own the test FavoriteNewsArticle
+		$this->newsArticle = new NewsArticle(null, "@phpunit", "test@phpunit.de", "+12125551212");
+		$this->newsArticle->insert($this->getPDO());
+
+		// calculate the date (just use the time the unit test was setup...)
+		$this->VALID_FAVORITENEWSARTICLEDATETIME = new \DateTime();
+	}
 	
-	 */
+	/**
+	 * test inserting a valid FavoriteNewsArticle and verify that the actual mySQL data matches
+	 **/
+	public function  testInsertValidFavoriteNewsArticle() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("favoriteNewsArticle");
+		
+		//create a new FavoriteNewsArticle and insert into mySQL
+		$favoriteNewsArticle = new FavoriteNewsArticle(null, $this->user->getUserId(), $this->newsArticle->getNewsArticleId(), $this->VALID_FAVORITENEWSARTICLEDATETIME);
+		$favoriteNewsArticle->insert($this->getPDO());
+		
+		//grab the data from mySQL and enforce the fields match our expectations
+		$pdoFavoriteNewsArticle = FavoriteNewsArticle::getFavoriteNewsArticleByUserId($this->getPDO(), $favoriteNewsArticle->getUserId());
+		$this->assertEquals($favoriteNewsArticle + 1, $this->getConnection()->getRowCount("favoriteNewsArticle"));
+		$this->assertEquals($pdoFavoriteNewsArticle->getUserId(), $this->)
+	}
 }
