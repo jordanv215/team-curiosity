fea134a7e3eb2e8705ab5751c54337f8779cb956
Kai Garrott <garrottkai@gmail.com>
2016-05-09 14:21:18 -0600

    add constructor https://app.asana.com/0/117435667927136/119450510065342

--- public_html/php/classes/CommentImage.php
+++ public_html/php/classes/CommentImage.php
@@ -38,5 +38,46 @@ class CommentImage implements \JsonSerializable {
 	 * ID of the user who created the comment - a foreign key
 	 * @var $commentImageUserId
 	 */
+	private $commentImageUserId;
 
+	/**
+	 * Constructor for the image comment
+	 * @param int|null $commentImageId primary key assigned by mySQL; empty until inserted
+	 * @param string $commentImageContent content of the comment
+	 * @param \DateTime|null $commentImageDateTime timestamp of the comment as assigned by mySQL
+	 * @param int $commentImageImageId id of the image commented on
+	 * @param int $commentImageUserId id of the user who created the comment
+	 * @throws \InvalidArgumentException if entry is not valid
+	 * @throws \RangeException if entry is negative or too long
+	 * @throws \TypeError if type hint is violated
+	 * @throws \Exception if some other exception occurs
+	 */
+	public function __construct(int $newCommentImageId = null, string $newCommentImageContent, $newCommentImageDateTime = null, int $newCommentImageImageId; int $newCommentImageUserId) {
+		try {
+			$this->setCommentImageId($newCommentImageId);
+			$this->setCommentImageContent($newCommentImageContent);
+			$this->setCommentImageDateTime($newCommentImageDateTime);
+			$this->setCommentImageImageId($newCommentImageImageId);
+			$this->setCommentImageUserId($newCommentImageUserId);
+		} catch(\InvalidArgumentException $invalidArgument) {
+			// rethrow exception to the caller
+			throw(new \InvalidArgumentException($invalidArgument->getMessage(), 0, $invalidArgument));
+		} catch(\RangeException $range) {
+			// rethrow exception to the caller
+			throw(new \RangeException($range->getMessage(), 0, $range));
+		} catch(\TypeError $typeError) {
+			// rethrow exception to the caller
+			throw(new \TypeError($typeError->getMessage(), 0, $typeError));
+		} catch(\Exception $exception) {
+			// rethrow exception to the caller
+			throw(new \Exception($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * accessor method for commentImageId
+	 *
+	 * @return int|null value of commentImageId
+	 */
+	public function getCommentImageId()
 }
