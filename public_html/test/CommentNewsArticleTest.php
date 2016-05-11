d5d33251390ee6a8ee8c226ec88f6448ac8a1369
Anthony Williams <pull.rebase>
2016-05-10 21:29:38 -0600

    Now working on CommentNewsArticleTest file

---
+++ public_html/test/CommentNewsArticleTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Edu\Cnm\TeamCuriosity\Test;
+
+use Edu\Cnm\TeamCuriosity\{CommentNewsArticle};
+
+// grab the project test parameters
+require_once("TeamCuriosityTest.php");
+
+//grab the class under scrutiny
+require_once("../php/classes/Autoload.php");
+
+/**
+ * FULL PHPUnit test for the CommentNewsArticle class
+ *
+ * This is a complete PHPUnit test of the CommentNewsArticle class. It is complete because *ALL* mySQL/PDO enabled methods
+ * are tested for both invalid and valid inputs.
+ *
+ * @see CommentNewsArticle
+ * @author Anthony Williams <awilliams144@cnm.edu>
+ *
+ * */
+class CommentNewsArticleTest extends TeamCuriosityTest {
+	
+}
