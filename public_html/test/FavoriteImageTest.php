6c82940b9431d2b83a47d73cfe156a42467ef2dd
Kai Garrott <garrottkai@gmail.com>
2016-05-11 15:47:16 -0600

    edits

--- public_html/test/FavoriteImageTest.php
+++ public_html/test/FavoriteImageTest.php
@@ -2,14 +2,14 @@
 namespace Edu\Cnm\TeamCuriosity\Test;
 
 use Edu\Cnm\TeamCuriosity\{
-	User, image, favoriteImage
+	User, Image, FavoriteImage
 };
 
 //grab the test parameters
 require_once("TeamCuriosityTest.php");
 
 //grab the class under scrutiny
-require_once("../php/classes/Autoload.php");
+require_once(dirname(__DIR__) . "/php/classes/Autoload.php");
 
 /**
  * FULL PHPUnit test for the FavoriteImage class
@@ -23,18 +23,19 @@ require_once("../php/classes/Autoload.php");
  **/
 class FavoriteImageTest extends TeamCuriosityTest {
 	/**
-	 * timestamp of the FavoriteImage; this starts as null and is assigned later
-	 * @var \DateTime $VALID_FavoriteImageDateTime
-	 **/
-	protected $VALID_FavoriteImageDateTime = null;
-	/**
 	 * user that favorites the FavoriteImage; this is for foreign key relations
 	 * @var User $user
-	 *
+	 **/
+	 protected $user = null;
+	 /** 
 	 * image that is favorited; this is for foreign key relations
 	 * @var image $image
 	 **/
-	protected $user = null;
+	protected $VALID_FavoriteImageDateTime = null;
+	/**
+	 * timestamp of the FavoriteImage; this starts as null and is assigned later
+	 * @var \DateTime $VALID_FavoriteImageDateTime
+	 **/
 	protected $image = null;
 
 	/**
@@ -49,7 +50,7 @@ class FavoriteImageTest extends TeamCuriosityTest {
 		$this->user->insert($this->getPDO());
 
 		// create and insert an image to own the test favoriteImage
-		$this->image = new image(null, null, "This is a test", "/test/test");
+		$this->image = new Image(null, null, "This is a test", "/test/test");
 		$this->image->insert($this->getPDO());
 
 		// calculate the date (just use the time the unit test was setup...)
@@ -65,14 +66,14 @@ class FavoriteImageTest extends TeamCuriosityTest {
 		$numRows = $this->getConnection()->getRowCount("FavoriteImage");
 
 		//create a new FavoriteImage and insert into mySQL
-		$FavoriteImage = new favoriteImage(null, $this->User->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
+		$FavoriteImage = new FavoriteImage(null, $this->User->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
 		$FavoriteImage->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce the fields match our expectations
-		$pdoFavoriteImage = favoriteImage::getFavoriteImageByUserId($this->getPDO(), $FavoriteImage->getUserId());
+		$pdoFavoriteImage = FavoriteImage::getFavoriteImageByUserId($this->getPDO(), $FavoriteImage->getUserId());
 		$this->assertEquals($FavoriteImage + 1, $this->getConnection()->getRowCount("FavoriteImage"));
 		$this->assertEquals($pdoFavoriteImage->getUserId(), $this->User->getUserId());
-		$this->assertEquals($pdoFavoriteImage->getImageId(), $this->image->getImageId());
+		$this->assertEquals($pdoFavoriteImage->getImageId(), $this->Image->getImageId());
 		$this->assertEquals($pdoFavoriteImage->getImageDateTime(), $this->VALID_FavoriteImageDateTime);
 	}
 
@@ -80,10 +81,10 @@ class FavoriteImageTest extends TeamCuriosityTest {
 	/**
 	 * PDO Exception
 	 */
-publc function testInsertInvalidFavoriteImage() {
-	$favoriteImage = new favoriteImage(TeamCuriosityTest::INVALID_KEY, $this->User->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
-	$favoriteImage->insert($this->getPDO());
-}
+	public function testInsertInvalidFavoriteImage() {
+		$favoriteImage = new FavoriteImage(TeamCuriosityTest::INVALID_KEY, $this->User->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
+		$favoriteImage->insert($this->getPDO());
+	}
 
 	/**
 	 * test to create favoriteImage and then delete it
@@ -113,7 +114,7 @@ publc function testInsertInvalidFavoriteImage() {
 	 * @expectedException \PDOException
 	 */
 	public function testGetInvalidFavoriteImage() {
-		$favoriteImage = new favoriteImage(null, $this->user->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
+		$favoriteImage = new FavoriteImage(null, $this->user->getUserId(), $this->image->getImageId(), $this->VALID_FavoriteImageDateTime);
 		$favoriteImage->delete($this->getPDO());
 
 	}
@@ -124,7 +125,7 @@ publc function testInsertInvalidFavoriteImage() {
 	 **/
 	public function testGetInvalidFavoriteImageByFavoriteImageImageIdAndFavoriteImageUserId() {
 		//grab a FavoriteImageImageIdAndFavoriteImageUserId that exceeds the maximum allowable favoriteImageImageIdAndFavoriteImageUserId
-		$favoriteImage = favoriteImage:: getFavoriteImageByFavoriteImageImageIdAndFavoriteImageUserId($this->getPDO(), TeamCuriosityTest::INVALID_KEY);
+		$favoriteImage = FavoriteImage:: getFavoriteImageByFavoriteImageImageIdAndFavoriteImageUserId($this->getPDO(), TeamCuriosityTest::INVALID_KEY);
 		$this->assertNull($favoriteImage);
 	}
 
@@ -143,7 +144,7 @@ publc function testInsertInvalidFavoriteImage() {
 	 */
 	public function testGetInvalidFavoriteImageByFavoriteImageUserId() {
 		//grab a favoriteImageUserId that exceeds max allowed amount
-		$favoriteImage = favoriteImage::getFavoriteImageByFavoriteImageUserId($this->getPDO(),TeamCuriosityTest::INVALID_KEY);
+		$favoriteImage = favoriteImage::getFavoriteImageByFavoriteImageUserId($this->getPDO(), TeamCuriosityTest::INVALID_KEY);
 		$this->assertNull($favoriteImage);
 	}
 
@@ -171,7 +172,6 @@ publc function testInsertInvalidFavoriteImage() {
 		$this->assertEquals($pdoFavoriteImage->getFavoriteImageDateTime(), $this->VALID_FavoriteImageDateTime);
 
 	}
-
 
 
 }
--- public_html/test/UserTest.php
+++ public_html/test/UserTest.php
@@ -23,15 +23,15 @@ class UserTest extends TeamCuriosityTest {
 	 * user
 	 * @var string $VALID_USER
 	 */
-	protected $VALID_EMAIL = "PHPUnit test passing";
-	protected $VALID_EMAIL2 = "PHPUnit test passing again";
+	protected $VALID_EMAIL = "testtest@test.test";
+	protected $VALID_EMAIL2 = "anothertest@test.test";
 	/**
 	 * user
 	 * @var string $VALID_USER2
 	 **/
-	protected $VALID_USERNAME = "PHPUnit test still passing";
+	protected $VALID_USERNAME = "Test User Name";
 
-	protected $LoginSource = null;
+	protected $loginSource = null;
 
 	//create dependent objects before running each test
 
@@ -40,48 +40,46 @@ class UserTest extends TeamCuriosityTest {
 		parent::setUp();
 
 		//create and insert a LoginSource to own the test user
-		$this->LoginSource = new LoginSource(null, "@phpunit", "@testphpunit.de", "12125551212");
-		$this->LoginSource->insert($this->getPDO());
-
-		$this->VALID_EMAIL = new \User();
+		$this->loginSource = new LoginSource(null, "27dollars", "Bernbook");
+		$this->loginSource->insert($this->getPDO());
 
 	}
 
 	//test inserting a valid email and verify that it matches the mySQL data
-	public function testInsertValidUser($pdoUserName, $pdoLoginSource) {
+	public function testInsertValidUser() {
 		//count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("Email");
+		$numRows = $this->getConnection()->getRowCount("User");
 
 		//create a new user and insert it into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_USERNAME);
-		$User->insert($this->getPDO());
+		$user = new User(null, $this->VALID_EMAIL, $this->loginSource->getLoginSourceId(), $this->VALID_USERNAME);
+		$user->insert($this->getPDO());
 
 		//grab the data from mySQL and enforce the fields to match our expectations
-		$pdoUser = User::getUserbyUserID($this->getPDO(), $User->getUserId());
+		$pdoUser = User::getUserbyUserId($this->getPDO(), $this->user->getUserId());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
-		$this->assertEquals($pdoUser->getEmail(), $this->User->getEmail());
-		$this->assertEquals($pdoUserName->getUserName(), $this->userName->getUsername());
-		$this->assertEquals($pdoLoginSource->getLoginSource(), $this->LoginSource->getLoginSource());
+		$this->assertEquals($pdoUser->getUserEmail(), $this->user->getUserEmail());
+		$this->assertEquals($pdoUser->getUserName(), $this->user->getUsername());
+		$this->assertEquals($pdoLoginSource->getLoginSource(), $this->loginSource->getLoginSource());
 
 	}
 
 	//test inserting something that already exists
 	//expecting PDOException
 
-	public function testInsertInvalidUser($getUserId) {
-		$User = new User(UserTest::INVALID_KEY, $this->User->$getUserId(), $this->VALID_EMAIL, $this->VALID_USERNAME);
-		$User->insert($this->getPDO());
+	public function testInsertInvalidUser() {
+		$user = new User(UserTest::INVALID_KEY, $this->user->getUserId(), $this->VALID_EMAIL, $this->VALID_USERNAME);
+		$user->insert($this->getPDO());
 
 	}
 
 	//test creating a user, editing it, and then updating it
-	public function testUpdateValidUser($User, $pdoUser) {
+	public function testUpdateValidUser() {
 		//count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("user");
+		$numRows = $this->getConnection()->getRowCount("User");
 
 		//create a new user and update it into mySQL
-		$User->setUserbyUserId($this->getPDO(), $User->getUserId());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("user"));
+		$user->setUserbyUserId($this->getPDO(), $user->getUserId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
 		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
 		$this->assertEquals($pdoUser->getEmail(), $this->VALID_EMAIL);
 		$this->assertEquals($pdoUser->getLoginSource(), $this->VALID_LOGINSOURCE);
@@ -91,9 +89,9 @@ class UserTest extends TeamCuriosityTest {
 
 //test updating a user that already exists
 //@expectedException PDOException
-	public function testUpdateInvalidUser($User) {
+	public function testUpdateInvalidUser() {
 		//create a user with a non null user id to watch it fail
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
+		$user = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
 		$User->update($this->getPDO());
 
 }
@@ -105,25 +103,25 @@ class UserTest extends TeamCuriosityTest {
 		$numRows = $this->getConnection()->getRowCount("User");
 
 		//create a new User and insert into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
-		$User->insert($this->getPDO());
+		$user = new User(null, $this->user->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
+		$user->insert($this->getPDO());
 
 		//delete this User from mySQL
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
-		$User->delere($this->getPDO());
+		$user->delete($this->getPDO());
 
 		//grab the data from mySQL and enforce the User does not exist
-		$pdoUser = User::GetUserbyUserId($this->getPDO(), $User->getUserId());
-		$this->assertNul($pdoUser);
-		$User->assertEquals($numRows, $this->User->getConnection()->getRowCount("User"));
+		$pdoUser = User::getUserByUserId($this->getPDO(), $user->getUserId());
+		$this->assertNull($pdoUser);
+		$user->assertEquals($numRows, $this->user->getConnection()->getRowCount("User"));
 
 	}
 
 	//test grabbing a User that does not exist
-	public function testGetInvalidUserbyUserId() {
+	public function testGetInvalidUserByUserId() {
 		//grab a profile id that exceeds the maximum allowable profile id
-		$User = User::getUserbyUserId($this->getPDO(), UserTest::INVALID_KEY);
-		$this->assertNull($User);
+		$user = User::getUserByUserId($this->getPDO(), UserTest::INVALID_KEY);
+		$this->assertNull($user);
 
 	}
 
@@ -133,18 +131,18 @@ class UserTest extends TeamCuriosityTest {
 		$numRows = $this->getConnection()->getRowCount("User");
 
 		// create a new User and insert to into mySQL
-		$tweet = new User(null, $this->UserEmail->getUserEmailId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
-		$tweet->insert($this->getPDO());
+		$user = new User(null, $this->userEmail->getUserEmailId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
+		$user->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
-		$results = User::getUserByUserId($this->getPDO(), $User->getUserEmail());
+		$results = User::getUserByUserId($this->getPDO(), $this->user->getUserEmail());
 		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
 		$this->assertCount(1, $results);
 		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\TeamCuriosity\\Test\\UserTest", $results);
 
 		// grab the result from the array and validate it
 		$pdoUser = $results[0];
-		$this->assertEquals($pdoUser->getUserId(), $this->User->getUserId());
+		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
 		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_EMAIL);
 		$this->assertEquals($pdoUser->getLoginSource(), $this->VALID_LOGINSOURCE);
 
@@ -155,8 +153,8 @@ class UserTest extends TeamCuriosityTest {
 	*/
 	public function testGetInvalidUserbyUserId() {
 		// grab a user by searching for content that does not exist
-		$User = User::getUserByUserId($this->getPDO(), "User not found");
-		$this->assertCount(0, $User);
+		$user = User::getUserByUserId($this->getPDO(), "User not found");
+		$this->assertCount(0, $user);
 	}
 
 	//test grabbing all users
@@ -165,18 +163,18 @@ class UserTest extends TeamCuriosityTest {
 		$numRows = $this->getConnection()->getRowCount("User");
 
 		// create a new User and insert to into mySQL
-		$User = new User(null, $this->User->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
-		$User->insert($this->getPDO());
+		$user = new User(null, $this->user->getUserId(), $this->VALID_EMAIL, $this->VALID_LOGINSOURCE);
+		$user->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
 		$results = User::getAllUsers($this->getPDO());
-		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("Users"));
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("User"));
 		$this->assertCount(1, $results);
 		$this->assertContainsOnlyInstancesOf("Edu\\Cnm\\TeamCuriosity\\Test\\UserTest", $results);
 
 		// grab the result from the array and validate it
 		$pdoUser = $results[0];
-		$this->assertEquals($pdoUser->getUserId(), $this->User->getUserId());
+		$this->assertEquals($pdoUser->getUserId(), $this->user->getUserId());
 		$this->assertEquals($pdoUser->getUserEmail(), $this->VALID_EMAIL);
 		$this->assertEquals($pdoUser->getLoginSource(), $this->VALID_LOGINSOURCE);
 	}
