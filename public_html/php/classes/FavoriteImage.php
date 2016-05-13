ee94c41f1b8e377ef84965268d34eedb459de061
Jordan Vinson <jvinson3@cnm.edu>
2016-05-13 10:46:34 -0600

    Group scrum, fixed all errors https://app.asana.com/0/117435667927136/119450510065344

--- public_html/php/classes/FavoriteImage.php
+++ public_html/php/classes/FavoriteImage.php
@@ -257,20 +257,20 @@ class FavoriteImage implements \JsonSerializable {
 		$statement->execute($parameters);
 
 		//build an array of favorite images
-			$favoriteImages = new \SplFixedArray($statement->rowCount());
-			$statement->setFetchMode(\PDO::FETCH_ASSOC);
-			while(($row = $statement->fetch()) !== false) {
-				try {
-					$favoriteImage = new favoriteImage($row["favoriteImageImageId"], $row["favoriteImageUserId"], $row["favoriteImageDateTime"]);
-					$favoriteImages[$favoriteImages->key()] = $favoriteImage;
-					$favoriteImages->next();
-				} catch(\Exception $exception) {
-					// if the row couldn't be converted, rethrow it
-					throw(new \PDOException($exception->getMessage(), 0, $exception));
-				}
+		$favoriteImages = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$favoriteImage = new favoriteImage($row["favoriteImageImageId"], $row["favoriteImageUserId"], $row["favoriteImageDateTime"]);
+				$favoriteImages[$favoriteImages->key()] = $favoriteImage;
+				$favoriteImages->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
 			}
-			return ($favoriteImages);
 		}
+		return ($favoriteImages);
+	}
 
 
 	/** get favoriteImage by favoriteImageByFavoriteImageUserId
@@ -303,15 +303,16 @@ class FavoriteImage implements \JsonSerializable {
 				$favoriteImages{$favoriteImages->key()} = $favoriteImage;
 				$favoriteImages->next();
 			}
-			}
+
 	catch
 		(\Exception $exception) {
 			// if row couldn't be converted, rethrow it
 			throw(new \PDOException($exception->getMessage(), 0, $exception));
 		}
 
-			return($favoriteImages);
+			return ($favoriteImages);
 		}
+}
 
 
 	/**
