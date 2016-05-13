835cf282de4a9d5c5f3cbd7d9e03891f05d2c4e2
Anthony Williams <pull.rebase>
2016-05-13 09:07:32 -0600

    Fixed more errors!

--- public_html/test/CommentImageTest.php
+++ public_html/test/CommentImageTest.php
@@ -16,38 +16,38 @@ require_once(dirname(__DIR__) . "/php/classes/Autoload.php");
  * 
  * @see CommentImage
  * @author Kai Garrott <garrottkai@gmail.com>
- */
+ **/
 
 class CommentImageTest extends TeamCuriosityTest {
 	/**
 	 * content of the image comment
 	 * @var string $VALID_COMMENT_IMAGE_CONTENT
-	 */
+	 **/
 	protected $VALID_COMMENT_IMAGE_CONTENT = "You are experiencing incredible success";
 	/**
 	 * content of the updated image comment
 	 * @var string $VALID_COMMENT_IMAGE_CONTENT2
-	 */
+	 **/
 	protected $VALID_COMMENT_IMAGE_CONTENT2 = "More incredible success";
 	/**
 	 * timestamp of the image comment - assigned by mySQL
 	 * @var \DateTime $VALID_COMMENT_IMAGE_DATE_TIME
-	 */
+	 **/
 	protected $VALID_COMMENT_IMAGE_DATE_TIME = null;
 	/**
 	 * user that created the comment - a foreign key relation
 	 * @var user User
-	 */
+	 **/
 	protected $user = null;
 	/**
 	 * photo that was commented on; foreign key relation
 	 * @var image Image
-	 */
+	 **/
 	protected $image = null;
 
 	/**
 	 * create dependent objects before running test
-	 */
+	 **/
 	public final function setUp() {
 		// runs the default setUp() method first
 		parent::setUp();
@@ -62,7 +62,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test inserting a valid image comment and verifying that the mySQL data matches
-	 */
+	 **/
 	public function testInsertValidCommentImage() {
 		// count the number of rows
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
@@ -84,7 +84,7 @@ class CommentImageTest extends TeamCuriosityTest {
 	 * test inserting a comment that already exists
 	 * 
 	 * @expectedException \PDOException
-	 */
+	 **/
 	public function testInsertInvalidCommentImage() {
 		// create an image comment with a non-null id; it should fail
 		$commentImage = new CommentImage(TeamCuriosityTest::INVALID_KEY, $this->VALID_COMMENT_IMAGE_CONTENT, $this->VALID_COMMENT_IMAGE_DATE_TIME, $this->image->getImageId(), $this->user->getUserId());
@@ -93,7 +93,7 @@ class CommentImageTest extends TeamCuriosityTest {
 	
 	/**
 	 * test inserting an image comment, editing it, and then updating it
-	 */
+	 **/
 	public function testUpdateValidCommentImage() {
 		// count number of rows in table
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
@@ -119,7 +119,7 @@ class CommentImageTest extends TeamCuriosityTest {
 	 * test updating an image comment that already exists
 	 *
 	 * @expectedException \PDOException
-	 */
+	 **/
 	public function testUpdateInvalidCommentImage() {
 		// create an image comment with a non-null id; it should fail
 		$commentImage = new CommentImage($this->TeamCuriosityTest->INVALID_KEY, $this->VALID_COMMENT_IMAGE_CONTENT, $this->VALID_COMMENT_IMAGE_DATE_TIME, $this->image->getImageId(), $this->user->getUserId());
@@ -128,7 +128,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test creating an image comment and then deleting it
-	 */
+	 **/
 	public function testDeleteValidCommentImage() {
 		// count number of table rows
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
@@ -151,7 +151,7 @@ class CommentImageTest extends TeamCuriosityTest {
 	 * test deleting an image comment that does not exist
 	 *
 	 * @expectedException \PDOException
-	 */
+	 **/
 	public function testDeleteInvalidCommentImage() {
 		// create a comment and try to delete it without actually inserting it
 		$commentImage = new CommentImage(null, $this->VALID_COMMENT_IMAGE_CONTENT, $this->VALID_COMMENT_IMAGE_DATE_TIME, $this->image->getImageId(), $this->user->getUserId());
@@ -160,7 +160,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test inserting an image comment and then regrabbing it from DB
-	 */
+	 **/
 	public function testGetValidCommentImageByCommentImageId() {
 		// count number of rows
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
@@ -180,7 +180,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test grabbing an image comment that does not exist
-	 */
+	 **/
 	public function testGetInvalidCommentImageByCommentImageId() {
 		// grab a profile id that is out of range
 		$commentImage = CommentImage::getCommentImageByCommentImageId($this->getPDO(), TeamCuriosityTest::INVALID_KEY);
@@ -189,7 +189,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test grabbing an image comment by image comment content
-	 */
+	 **/
 	public function testGetValidCommentImageByCommentImageContent() {
 		// count number of rows
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
@@ -214,7 +214,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test grabbing an image comment by content that does not exist
-	 */
+	 **/
 	public function testGetInvalidCommentImageByCommentImageContent() {
 		// grab an image comment by searching for content that does not exist
 		$commentImage = CommentImage::getCommentImageByCommentImageContent($this->getPDO(), "LOL good luck");
@@ -223,7 +223,7 @@ class CommentImageTest extends TeamCuriosityTest {
 
 	/**
 	 * test grabbing all image comments
-	 */
+	 **/
 	public function testGetAllCommentImage() {
 		// count number of table rows
 		$numRows = $this->getConnection()->getRowCount("CommentImage");
