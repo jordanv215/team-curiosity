24ce77eb28af12c4af5025a801dcb61587264205
Jordan Vinson <jvinson3@cnm.edu>
2016-05-23 16:12:21 -0600

    Fixed autoload

--- public_html/php/apis/commentimage/index.php
+++ public_html/php/apis/commentimage/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once "autoloader.php";
+require_once "../../classes/Autoload.php";
 require_once "/lib/xsrf.php";
 require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
 
