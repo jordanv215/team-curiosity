4fdc7679e537ec5d3a4d0cd09838018fa729e215
Kai Garrott <garrottkai@gmail.com>
2016-05-23 22:06:54 -0600

    api complete for now, minus mysterious error re: bracing https://app.asana.com/0/117435667927136/135701481524164

--- public_html/php/apis/loginsource/index.php
+++ public_html/php/apis/loginsource/index.php
@@ -33,9 +33,10 @@ try {
 		$loginSourceId = filter_input(INPUT_GET, "loginSourceId", FILTER_VALIDATE_INT);
 
 		// ensure the input is valid
-		if(($method === "DELETE" || $method === "PUT") && (empty($loginSourceId) === true || $loginSourceId <= 0) {
-			throw(new \InvalidArgumentException("input is empty or invalid"));
+		if(($method === "DELETE" || $method === "PUT") && (empty($loginSourceId) === true || $loginSourceId <= 0)) {
+			throw(new \InvalidArgumentException("Input is empty or invalid"));
 		}
+
 
 		// handle GET request: return login source associated with the id; if none specified, return all
 		if($method === "GET") {
@@ -62,6 +63,67 @@ try {
 			$requestObject = json_decode($requestContent);
 
 			// make sure login source api key is present
-			
-		}
-}
+			if(empty($requestObject->loginSourceApiKey) === true) {
+				throw(new \InvalidArgumentException("No api key to update"));
+			}
+
+			if($method === "PUT") {
+
+				// retrieve the login source to update
+				$loginSource = TeamCuriosity\LoginSource::getLoginSourceByLoginSourceId($pdo, $loginSourceId);
+				if($loginSource === null) {
+					throw(new \RuntimeException("No valid login source to update"));
+				}
+
+				// add the new data and update
+				$loginSource->setLoginSourceApiKey($requestObject->loginSourceApiKey);
+				$loginSource->update($pdo);
+
+				// update the reply
+				$reply->message = "Login source successfully updated";
+
+			} else if($method === "POST") {
+
+				// create new login source and insert into table
+				$loginSource = new TeamCuriosity\LoginSource(null, $requestObject->loginSourceApiKey, $requestObject->loginSourceProvider);
+				$loginSource->insert($pdo);
+
+				// update the reply
+				$reply->message = "Login source successfully created";
+
+			} else if($method === "DELETE") {
+				verifyXsrf();
+
+				// retrieve the login source to delete
+				$loginSource = TeamCuriosity\LoginSource::getLoginSourceByLoginSourceId($pdo, $loginSourceId);
+				if($loginSource === null) {
+					throw(new \RuntimeException("No valid login source to delete"));
+				}
+
+				// delete login source
+				$loginSource->delete($pdo);
+
+				// update the reply
+				$reply->message = "Login source successfully deleted";
+			} else {
+				throw(new \InvalidArgumentException("Invalid HTTP method request"));
+			}
+
+			// update the reply with exception information
+		} catch(Exception $exception) {
+			$reply->status = $exception->getCode();
+			$reply->message = $exception->getMessage();
+			$reply->trace = $exception->getTraceAsString();
+	} catch(TypeError $typeError) {
+			$reply->status = $typeError->getCode();
+			$reply->message = $typeError->getMessage();
+	}
+
+	header("Content-type: application/json");
+	if($reply->data === null) {
+		unset($reply->data);
+	}
+	
+	// encode and return the reply to the frontend caller
+	echo json_encode($reply);
+
