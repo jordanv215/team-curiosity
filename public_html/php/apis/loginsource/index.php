a295416839c82c22d95453d99806e298ad596b30
Kai Garrott <garrottkai@gmail.com>
2016-05-23 21:15:38 -0600

    update LoginSource api https://app.asana.com/0/117435667927136/135701481524164

--- public_html/php/apis/loginsource/index.php
+++ public_html/php/apis/loginsource/index.php
@@ -29,5 +29,39 @@ try {
 		// determine the HTTP method used
 		$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
 
+		// sanitize input
+		$loginSourceId = filter_input(INPUT_GET, "loginSourceId", FILTER_VALIDATE_INT);
 
+		// ensure the input is valid
+		if(($method === "DELETE" || $method === "PUT") && (empty($loginSourceId) === true || $loginSourceId <= 0) {
+			throw(new \InvalidArgumentException("input is empty or invalid"));
+		}
+
+		// handle GET request: return login source associated with the id; if none specified, return all
+		if($method === "GET") {
+
+			// set XSRF cookie
+			setXsrfCookie();
+
+			// grab the specified login source, or all login sources, and update reply
+			if(empty($loginSourceId) === false) {
+				$loginSource = TeamCuriosity\LoginSource::getLoginSourceByLoginSourceId($pdo, $loginSourceId);
+				if($loginSource !== null) {
+					$reply->data = $loginSource;
+				}
+			} else {
+				$loginSources = TeamCuriosity\LoginSource::getAllLoginSources($pdo);
+				if($loginSources !== null) {
+					$reply->data = $loginSources;
+				}
+			}
+
+		} else if($method === "PUT" || $method === "POST") {
+			verifyXsrf();
+			$requestContent = file_get_contents("php://input");
+			$requestObject = json_decode($requestContent);
+
+			// make sure login source api key is present
+			
+		}
 }
