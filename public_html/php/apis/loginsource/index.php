dac43a4dace511cf42d96615eea04d60ce5cbf7d
Kai Garrott <garrottkai@gmail.com>
2016-05-25 13:57:34 -0600

    fix include filepaths

--- public_html/php/apis/loginsource/index.php
+++ public_html/php/apis/loginsource/index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once "../../classes/Autoload.php";
-require_once "file/path/here"; // @TODO add xsrf.php file here ---------------------------------------
-require_once "/etc/apache2/capstone-mysql/encrypted-config.php";
+require_once(dirname(__DIR__, 2) . "/classes/Autoload.php");
+require_once(dirname(__DIR__, 2) . "/lib/xsrf.php");
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
 use Edu\Cnm\TeamCuriosity;
 
