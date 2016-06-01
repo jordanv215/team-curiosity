088f3637a614d034af5ea57a5c1c59a056b20033
Kai Garrott <garrottkai@gmail.com>
2016-06-01 16:52:37 -0600

    update weather api

--- public_html/php/apis/weather/index.php
+++ public_html/php/apis/weather/index.php
@@ -3,11 +3,14 @@
 $str = file_get_contents("http://cab.inta-csic.es/rems/wp-content/plugins/marsweather-widget/api.php");
 // decode the JSON into an associative array
 $json = json_decode($str, true);
-echo "<pre>".print_r($json, true)."</pre>";
-$terrestrial_date = $json["soles"][0]["terrestrial_date"];
-$sol = $json["soles"][0]["sol"];
-$min_temp = $json["soles"][0]["min_temp"];
-$max_temp = $json["soles"][0]["max_temp"];
-$pressure = $json["soles"][0]["pressure"];
-$atmo_opacity = $json["soles"][0]["atmo_opacity"];
+$result = new stdClass();
+//echo "<pre>".print_r($json, true)."</pre>";
+$result->terrestrial_date = $json["soles"][0]["terrestrial_date"];
+$result->sol = $json["soles"][0]["sol"];
+$result->min_temp = $json["soles"][0]["min_temp"];
+$result->max_temp = $json["soles"][0]["max_temp"];
+$result->pressure = $json["soles"][0]["pressure"];
+$result->atmo_opacity = $json["soles"][0]["atmo_opacity"];
 
+header("Content-type: application/json");
+echo json_encode($result);
