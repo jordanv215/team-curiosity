b528c2a96cabc17c788b9045683d49da1a1720b9
Jordan Vinson <jvinson3@cnm.edu>
2016-06-06 16:05:22 -0600

    Added news carousel to the news-carousel-placeholder doc https://app.asana.com/0/117435667927136/118836678397155

---
+++ placeholder-directory/news-carousel-placeholder.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>RedRovr.io</title>
+
+		<meta charset="utf-8">
+
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+				integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+		<!-- fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css">
+
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!--[if lt IE 9]>
+		<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif]-->
+
+		<!-- CUSTOM CSS -->
+		<link rel="stylesheet" href="css/style.css" type="text/css">
+
+		<!-- jQuery -->
+		<script src="https://code.jquery.com/jquery-2.2.3.min.js"
+				  integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
+
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
+	</head>
+
+	<body>
+
+
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	<!-- Indicators -->
+	<ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	</ol>
+
+	<!-- Wrapper for slides -->
+	<div class="carousel-inner" role="listbox">
+		<div class="item active">
+			<div class="carousel-header">
+				<h3>Trump Elected God-King of Mars</h3>
+				<p>After a full year of violent outbursts and downright stupidity, somehow, Trump has been elected the God-King of the planet Mars. Only the future will tell whether this was a good choice or not for the Martian people. (click here to read more)</p>
+			</div>
+		</div>
+
+		<div class="item">
+			<div class="carousel-header">
+				<h3>Trump Builds Giant Wall Around Mars-Phobos Space Border</h3>
+				<p>Phobosians riot as Trump makes their own president pay for the wall that completely blocks them off from Mars. God-King Trump has not commented on the situation, but did ask for our passports when we told him we were from New Marsico. (click here to read more)</p>
+			</div>
+		</div>
+
+		<div class="item">
+			<div class="carousel-header">
+				<h3>Tragedy on Mars</h3>
+				<p>Tragedy struck on Mars today as Trump, the beloved God-King of the planet, passed away. Sources say his heart stopped, although there were no signs of heart attack, or any indication that he was sick. (click to read more)</p>
+			</div>
+		</div>
+
+		<div class="item">
+			<div class="carousel-header">
+				<h3>Autopsy Shows God-King Trump's Heart Caused by 100% Solid Stone Heart</h3>
+				<p>3 days after the death of Trump was announced, the autopsy has finally finished, concluding that the widely popular God-King had a sort of heart attack when his heart turned completely to stone. Sources and witnesses say that Trump was in the middle of arguing with the homeless about why a $1 million dollar loan is a regular thing, when he dropped and never moved again. (click here to read more)</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Left and right controls -->
+	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+		<span class="sr-only">Previous</span>
+	</a>
+	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		<span class="sr-only">Next</span>
+	</a>
+</div>
+	</body>
+</html>
