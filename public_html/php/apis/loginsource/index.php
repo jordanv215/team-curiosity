e750b8f9b7861579dbb009902a808318a62b1fc0
Jordan Vinson <jvinson3@cnm.edu>
2016-06-06 10:06:08 -0600

    Added code to LoginSource

--- public_html/php/apis/loginsource/index.php
+++ public_html/php/apis/loginsource/index.php
@@ -16,6 +16,69 @@ use Edu\Cnm\TeamCuriosity;
 if(session_status() !== PHP_SESSION_ACTIVE) {
 	session_start();
 }
+//this } might cause a problem, documentation doesn't show there there is a brace in between session start and the body of the source.
+	$provider = new \League\OAuth2\Client\Provider\Facebook([
+		'clientId'          => '{facebook-app-id}',
+		'clientSecret'      => '{facebook-app-secret}',
+		'redirectUri'       => 'https://example.com/callback-url',
+		'graphApiVersion'   => 'v2.6',
+	]);
+
+	if (!isset($_GET['code'])) {
+
+		// If we don't have an authorization code then get one
+		$authUrl = $provider->getAuthorizationUrl([
+			'scope' => ['email', '...', '...'],
+		]);
+		$_SESSION['oauth2state'] = $provider->getState();
+
+		echo '<a href="'.$authUrl.'">Log in with Facebook!</a>';
+		exit;
+
+// Check given state against previously stored one to mitigate CSRF attack
+	} elseif (empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+
+		unset($_SESSION['oauth2state']);
+		echo 'Invalid state.';
+		exit;
+
+	}
+
+// Try to get an access token (using the authorization code grant)
+	$token = $provider->getAccessToken('authorization_code', [
+		'code' => $_GET['code']
+	]);
+
+// Optional: Now you have a token you can look up a users profile data
+	try {
+
+		// We got an access token, let's now get the user's details
+		$user = $provider->getResourceOwner($token);
+
+		// Use these details to create a new profile
+		printf('Hello %s!', $user->getFirstName());
+
+		echo '<pre>';
+		var_dump($user);
+		# object(League\OAuth2\Client\Provider\FacebookUser)#10 (1) { ...
+		echo '</pre>';
+
+	} catch (\Exception $e) {
+
+		// Failed to get user details
+		exit('Oh dear...');
+	}
+
+	echo '<pre>';
+// Use this to interact with an API on the users behalf
+	var_dump($token->getToken());
+# string(217) "CAADAppfn3msBAI7tZBLWg...
+
+// The time (in epoch time) when an access token will expire
+	var_dump($token->getExpires());
+# int(1436825866)
+	echo '</pre>';
+
 
 // create an empty reply
 $reply = new stdClass();
