4ba73b1261bda457d4bae46cdc2589f4a0ac8aa4
Ellen Liu <huailun@hotmail.com>
2016-06-07 10:10:33 -0600

    added image-random-placeholder.php

--- public_html/php/templates/head-utils.php
+++ public_html/php/templates/head-utils.php
@@ -31,7 +31,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 
 		<!--Angular JS Libraries-->
-		<?php $ANGULAR_VERSION = "1.5.7";?>
+		<?php $ANGULAR_VERSION = "1.5.6";?>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-messages.min.js"></script>
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-route.js"></script>
