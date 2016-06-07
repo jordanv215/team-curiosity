fb51b551ee61d7a705effa035b7ba5194e66fa5e
Ellen Liu <huailun@hotmail.com>
2016-06-07 15:37:36 -0600

    created route-config.js https://app.asana.com/0/117435667927136/118836678397155

---
+++ public_html/angular/route-config.js
@@ -0,0 +1,30 @@
+// configure our routes
+app.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+
+	// route for the home page
+		.when('/', {
+			controller  : 'homeController',
+			templateUrl : 'angular/templates/home.php'
+		})
+
+		// route for the images page
+		.when('/images', {
+			controller  : 'imagesController',
+			templateUrl : 'angular/templates/images.php'
+		})
+
+		// route for the news page
+		.when('/news', {
+			controller  : 'newsController',
+			templateUrl : 'angular/templates/news.php'
+		})
+
+		// otherwise redirect to home
+		.otherwise({
+			redirectTo: "/"
+		});
+
+	//use the HTML5 History API
+	$locationProvider.html5Mode(true);
+});
