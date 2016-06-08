ec25753ef26a1bbef9ca95a0f1f33b63be69adc9
Jordan Vinson <jvinson3@cnm.edu>
2016-06-08 14:55:57 -0600

    Created a login service https://app.asana.com/0/117435667927136/118836678397155

--- public_html/angular/controller/images-controller.js
+++ public_html/angular/controller/images-controller.js
@@ -1,3 +1,18 @@
-app.controller('imagesController', function($scope) {
-
-});
+app.controller("imagesController", ["$scope", "ImagesService", function($scope, ImagesService) {
+	$scope.images = [];
+	
+	$scope.getImages = function() {
+		ImagesService.fetchImage()
+			.then(function(result) {
+				if(result.status === 200) {
+					$scope.images = result.data;
+				} else {
+					console.log("couldn't load images: " + result.data.message);
+				}
+			});
+	};
+	// load the array on first view
+	if($scope.images.length === 0) {
+		$scope.images = $scope.getImages();
+	}
+}]);
