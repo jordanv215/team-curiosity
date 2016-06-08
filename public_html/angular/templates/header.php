97aea767d01464eef0c4565fd2af1ae6cbab910e
Jordan Vinson <jvinson3@cnm.edu>
2016-06-08 15:04:11 -0600

    Added php header template to angular https://app.asana.com/0/117435667927136/118836678397155

---
+++ public_html/angular/templates/header.php
@@ -0,0 +1,29 @@
+<header>
+	<nav class="navbar navbar-inverse">
+		<div class="container ng-1">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="#">REDROVR</a>
+			</div>
+			<div class="collapse navbar-collapse" id="myNavbar">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href data-toggle="modal" data-target="#about-modal">About</a></li>
+					<li><a>Images</a></li>
+					<li><a>News</a></li>
+					<li class="dropdown">
+						<a class="dropdown-toggle" data-toggle="dropdown" href="#">Search
+							<span class="caret"></span></a>
+						<div class="dropdown-menu">
+							search here
+						</div>
+				</ul>
+			</div>
+		</div>
+	</nav>
+</header>
+
+<?php require_once("../../php/templates/about-modal.php");?>
