7d1e47b527067db6fa2435590233c058b6d76aab
Jordan Vinson <jvinson3@cnm.edu>
2016-06-08 14:32:54 -0600

    added a header controller https://app.asana.com/0/117435667927136/118836678397155

---
+++ public_html/angular/controller/header-controller.js
@@ -0,0 +1,22 @@
+app.controller("HeaderController", ["$scope", "GoogleService", function($scope, GoogleService) {
+	$scope.header = [];
+
+	/**
+	 * fulfills the promise from retrieving the header and search bar data
+	 **/
+	$scope.searchHeader = function(name) {
+		GoogleService.fetchName(name)
+			.then(function(result) {
+				if(result.status === 200) {
+					$scope.header = result.data;
+				} else {
+					console.log("couldn't load the header data: " + result.data.message);
+				}
+			});
+	};
+
+	// load the array on first view
+	if($scope.header.length === 0) {
+		$scope.header = $scope.searchHeader();
+	}
+}]);
