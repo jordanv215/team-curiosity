b2e4b957ea7be6668b27155146aecb36f938ffdb
Jordan Vinson <jvinson3@cnm.edu>
2016-06-08 14:24:35 -0600

    Added header controller and modals https://app.asana.com/0/117435667927136/118836678397155

---
+++ public_html/php/templates/image-modal.php
@@ -0,0 +1,15 @@
+<div class="container">
+
+	<!-- Modal -->
+	<div class="modal fade" id="image-Modal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-body">
+					<img src="../../image/mars.png" alt="mars modal example" width="1024px" height="682px">
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ public_html/php/templates/search modal.php
@@ -0,0 +1,17 @@
+<div class="container">
+
+	<!-- Modal -->
+	<div class="modal fade" id="search-modal" role="dialog">
+		<div class="modal-dialog">
+
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-body">
+					<p>SEARCH SEARCH SEARCH SEARCHt</p>
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+</div>
