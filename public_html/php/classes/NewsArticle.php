4927334da48a801be985cd49fff963225c0b86fb
Kai Garrott <garrottkai@gmail.com>
2016-06-08 22:28:24 -0600

    add GetNewsArticleByNewsArticleUrl for api access https://app.asana.com/0/117435667927136/135701481524170

--- public_html/php/classes/NewsArticle.php
+++ public_html/php/classes/NewsArticle.php
@@ -480,6 +480,45 @@ class NewsArticle implements \JsonSerializable {
 	}
 
 	/**
+	 * gets the NewsArticle by URL
+	 * sanitization unnecessary because this method is strictly internal
+	 *
+	 * @param \PDO $pdo PDO connection object
+	 * @param string $newsArticleUrl News Article URL to search for
+	 * @return \SplFixedArray SplFixedArray of NewsArticles found
+	 * @throws \PDOException when mySQL related errors occur
+	 * @throws \TypeError when variables are not the correct data type
+	 **/
+	public static function getNewsArticleByNewsArticleUrl(\PDO $pdo, string $newsArticleUrl) {
+
+		// create query template
+		$query = "SELECT newsArticleId, newsArticleTitle, newsArticleDate, newsArticleSynopsis, newsArticleUrl FROM NewsArticle WHERE newsArticleUrl LIKE :newsArticleUrl";
+		$statement = $pdo->prepare($query);
+
+		// bind the newsArticleUrl to the place holder in the template
+		$newsArticleUrl = "%$newsArticleUrl%";
+		$parameters = array("newsArticleUrl" => $newsArticleUrl);
+		$statement->execute($parameters);
+		// build an array of NewsArticles
+		$newsArticles = new \SplFixedArray($statement->rowCount());
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		while(($row = $statement->fetch()) !== false) {
+			try {
+				$newsArticle = new NewsArticle($row["newsArticleId"], $row["newsArticleTitle"], \DateTime::createFromFormat("Y-m-d H:i:s", $row["newsArticleDate"]), $row["newsArticleSynopsis"], $row["newsArticleUrl"]);
+				$newsArticles[$newsArticles->key()] = $newsArticle;
+				$newsArticles->next();
+			} catch(\Exception $exception) {
+				// if the row couldn't be converted, rethrow it
+				throw(new \PDOException($exception->getMessage(), 0, $exception));
+			}
+		}
+
+		return ($newsArticles);
+
+	}
+	
+
+	/**
 	 * gets all NewsArticles
 	 *
 	 * @param \PDO $pdo PDO connection object
