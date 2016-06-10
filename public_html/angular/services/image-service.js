bb6ef090f89882aa7869727c818e2d17897ab0bf
Ellen Liu <huailun@hotmail.com>
2016-06-10 09:50:57 -0600

    edited services https://app.asana.com/0/117435667927136/118836678397155

--- public_html/angular/services/image-service.js
+++ public_html/angular/services/image-service.js
@@ -1,11 +1,18 @@
-app.constant("IMAGE_ENDPOINT", "../../php/apis/image/");
+app.constant("IMAGE_ENDPOINT", "php/apis/image/");
 app.service("ImageService", function($http, IMAGE_ENDPOINT) {
 	function getUrl() {
 		return(IMAGE_ENDPOINT);
 	}
-
-
-	this.fetchImage = function() {
-		return($http.get(getUrl()))
+	function getUrlForId(imageId) {
+		return(getUrl() + imageId);
 	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function(imageId) {
+		return($http.get(getUrlForId(imageId)));
+	};
+
 });
--- public_html/angular/services/news-service.js
+++ public_html/angular/services/news-service.js
@@ -1,8 +1,19 @@
-app.constant("NEWS_ENDPOINT", "../../php/apis/newsarticle/");
+app.constant("NEWS_ENDPOINT", "php/apis/newsarticle/");
 app.service("NewsService", function($http, NEWS_ENDPOINT) {
 	function getUrl() {
 		return(NEWS_ENDPOINT);
 	}
+	function getUrlForId(newsArticleId) {
+		return(getUrl() + newsArticleId);
+	}
+
+	this.all = function() {
+		return($http.get(getUrl()));
+	};
+
+	this.fetch = function(newsArticleId) {
+		return($http.get(getUrlForId(newsArticleId)));
+	};
 
 
 	this.fetchNews = function() {
