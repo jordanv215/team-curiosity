4c2b4da91e4abbeff9c80e5f228179068b2384fa
Ellen Liu <huailun@hotmail.com>
2016-06-09 20:21:08 -0600

    updates https://app.asana.com/0/117435667927136/118836678397155

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -2,25 +2,25 @@
 <div class="row" ng-if="weather == null">
 	<h2 class="text-center"><i class="fa fa-space-shuttle fa-spin" aria-hidden="true"></i> Loading weather data&hellip;</h2>
 </div>
-	<div class="row" ng-if="weather != null">
+	<div class="row" ng-if="weather != null" id="weather">
 		<div class="col-xs-12">
 			<div class="col-xs-3">
-				<div class="big-box">{{ weather.max_temp }} &deg;F</div>
+				<div class="big-box" id="temp">{{ weather.max_temp }} &deg;F</div>
 				<div class="mini-box">High</div>
 			</div><!-- column for max temperature-->
 			<div class="col-xs-3">
-				<div class="big-box">{{ weather.min_temp }} &deg;F</div>
+				<div class="big-box" id="temp">{{ weather.min_temp }} &deg;F</div>
 				<div class="mini-box">Low</div>
 			</div><!-- column for min_temp-->
 			<div class="col-xs-3">
-				<div class="mini-box">Sol {{ weather.sol }}</div>
-				<div class="row">{{ weather.pressure }} Pa</div>
-				<div class="mini-box">Pressure (Pa)</div>
+				<div class="mini-box" id="date">Sol {{ weather.sol }}</div>
+				<div class="row" id="pressure">{{ weather.pressure }} Pa</div>
+				<div class="mini-box" id="sky">Pressure</div>
 			</div>
 			<div class="col-xs-3">
-				<div class="mini-box">{{ weather.terrestrial_date }}</div>
-				<div class="row">{{ weather.atmo_opacity }}</div>
-				<div class="mini-box">Sky</div>
+				<div class="mini-box" id="date">{{ weather.terrestrial_date }}</div>
+				<div class="row" id="pressure">{{ weather.atmo_opacity }}</div>
+				<div class="mini-box" id="sky">Sky</div>
 			</div>
 		</div><!--main weather column -->
 	</div><!-- row for weather-->
