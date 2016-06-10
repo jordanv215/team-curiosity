9ed17b219361ebf4e13b8f1fc49ccba56d536799
Jordan Vinson <jvinson3@cnm.edu>
2016-06-10 09:48:27 -0600

    Added columbs to two equal carousels. Added content to image carousel

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -25,58 +25,78 @@
 		</div><!--main weather column -->
 	</div><!-- row for weather-->
 <!--these columns will contain our two content carousels. They aren't centered right now, but they're there for future needs and they're the same width.-->
-<div id="imageCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
-	</ol>
 
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner" role="listbox">
-		<div class="item active">
-			<img src="../public_html/image/mars.png" alt="Mars">
-		</div>
+<div class="col-md-6 col-md-6">
+	<div id="imageCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#imageCarousel" data-slide-to="0" class="active"></li>
+		</ol>
 
-	</div>
-
-	<!-- Left and right controls -->
-	<a class="left carousel-control" href="#imageCarousel" role="button" data-slide="prev">
-		<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-		<span class="sr-only">Previous</span>
-	</a>
-	<a class="right carousel-control" href="#imageCarousel" role="button" data-slide="next">
-		<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-		<span class="sr-only">Next</span>
-	</a>
-</div>
-
-			<!-- Wrapper for slides -->
-<div id="newsCarousel" class="carousel slide" data-ride="carousel">
-	<!-- Indicators -->
-	<ol class="carousel-indicators">
-		<li data-target="#newsCarousel" data-slide-to="0" class="active"></li>
-	</ol>
-
-	<!-- Wrapper for slides -->
-	<div class="carousel-inner" role="listbox">
-		<div class="item active">
-			<div class="carousel-header">
-				<h3>{{newsArticle.title}}</h3>
-				<p>{{newsArticle.description}}</p>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="../public_html/image/sunset.gif" alt="sunset">
 			</div>
+
+			<div class="item">
+				<img src="../public_html/image/mars-landscape.jpg" alt="mars-surface">
+			</div>
+			
+			<div class="item">
+				<img src="../public_html/image/above-shot.jpg" alt="above-shot">
+			</div>
+
+			<div class="item">
+				<img src="../public_html/image/mt-sharp" alt="mt-sharp">
+			</div>
+			
+			<div class="item">
+				<img src="../public_html/image/rover-selfie" alt="selfie">
+			</div>
+
 		</div>
 
 		<!-- Left and right controls -->
-		<a class="left carousel-control" href="#newsCarousel" role="button" data-slide="prev">
+		<a class="left carousel-control" href="#imageCarousel" role="button" data-slide="prev">
 			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 			<span class="sr-only">Previous</span>
 		</a>
-		<a class="right carousel-control" href="#newsCarousel" role="button" data-slide="next">
+		<a class="right carousel-control" href="#imageCarousel" role="button" data-slide="next">
 			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 			<span class="sr-only">Next</span>
 		</a>
 	</div>
+
+	<!-- Wrapper for slides -->
+	<div id="newsCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#newsCarousel" data-slide-to="0" class="active"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<div class="carousel-header">
+					<h3>{{newsArticle.title}}</h3>
+					<p>{{newsArticle.description}}</p>
+				</div>
+			</div>
+
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#newsCarousel" role="button" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#newsCarousel" role="button" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
+	</div>
 </div>
+
 
 	
 
