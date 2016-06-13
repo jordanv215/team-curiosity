86ded612b99675d365bb9b02efdcc842f2469ab8
Kai Garrott <garrottkai@gmail.com>
2016-06-13 13:48:53 -0600

    fix news carousel

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -123,11 +123,11 @@
 					</div>
 				</div>
 				<!-- Left and right controls -->
-				<a class="left carousel-control" href="javascript:void(0)" role="button" data-slide="prev" data-target="#imageCarousel">
+				<a class="left carousel-control" href="#imageCarouselSplash" role="button" data-slide="prev" data-target="#imageCarousel">
 					<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
 					<span class="sr-only">Previous</span>
 				</a>
-				<a class="right carousel-control" href="javascript:void(0)" role="button" data-slide="next" data-target="#imageCarousel">
+				<a class="right carousel-control" href="#imageCarouselSplash" role="button" data-slide="next" data-target="#imageCarousel">
 					<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 					<span class="sr-only">Next</span>
 				</a> 
