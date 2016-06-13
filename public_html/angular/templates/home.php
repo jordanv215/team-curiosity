4ecc4578056398d95651867dda87ef71e615d7c5
Jordan Vinson <jvinson3@cnm.edu>
2016-06-13 16:15:43 -0600

    Changed break point on the weather bar

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -6,23 +6,23 @@
 </div>
 <div class="container-fluid">
 	<div class="row" ng-if="weather != null" id="weather">
-		<div class="col-xs-6 col-sm-3">
+		<div class="col-xs-3">
 			<div class="big-box" id="temp">{{ weather.max_temp }} &deg;<small>F</small></div>
-			<div class="mini-box">High</div>
+			<div class="mini-box weather-label">High</div>
 		</div><!-- column for max temperature-->
-		<div class="col-xs-6 col-sm-3">
+		<div class="col-xs-3">
 			<div class="big-box" id="temp">{{ weather.min_temp }} &deg;<small>F</small></div>
-			<div class="mini-box">Low</div>
+			<div class="mini-box weather-label">Low</div>
 		</div><!-- column for min_temp-->
-		<div class="col-xs-6 col-sm-3">
+		<div class="col-xs-3">
 			<div class="mini-box" id="date">Sol {{ weather.sol }}</div>
 			<div class="row" id="pressure">{{ weather.pressure }} Pa</div>
-			<div class="mini-box">Pressure</div>
+			<div class="mini-box weather-label">Pressure</div>
 		</div>
-		<div class="col-xs-6 col-sm-3">
+		<div class="col-xs-3">
 			<div class="mini-box" id="date">{{ weather.terrestrial_date }}</div>
 			<div class="row" id="pressure">{{ weather.atmo_opacity }}</div>
-			<div class="mini-box">Sky</div>
+			<div class="mini-box weather-label">Sky</div>
 		</div>
 	</div><!-- row for weather-->
 </div>
