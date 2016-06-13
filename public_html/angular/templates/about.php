5fbf169ff4b137afe382f7a0ec977e637893bbfc
Kai Garrott <garrottkai@gmail.com>
2016-06-12 23:53:19 -0600

    add about page paragraphs

--- public_html/angular/templates/about.php
+++ public_html/angular/templates/about.php
@@ -6,8 +6,10 @@
 				<h3 class="panel-title">About <em>Curiosity</em></h3>
 			</div>
 			<div class="panel-body">
-				Curiosity is a car-sized, six-wheeled robot destined for Gale Crater on Mars.
-				Its mission: to see if Mars ever could have supported small life forms called microbes...and if humans could survive there someday! In addition to super-human senses that help us understand Mars as a habitat for life, Curiosity's parts are similar to what a human would need to explore Mars (body, brains, eyes, arm, legs, etc.). Check it out though--sometimes they are located in odd places!
+				<p><em>Curiosity,</em> officially known as the NASA Mars Science Laboratory, is an 1800-pound robotic vehicle currently exploring the surface of the red planet in search of life. The rover was launched from Earth on November 26, 2011, and touched down on August 6, 2012, after a journey of 350 million miles.</p>
+				<p>The area which <em>Curiosity</em> now calls home is part of Gale Crater, a 96-mile-wide impact remnant containing highly varied geology. Gale Crater was chosen as the target for the mission because it appears to have contained water eons ago.</p>
+				<p>The rover carries an array of scientific instruments designed to analyze the structural and chemical composition of the sand and rock covering the floor of the crater. This information continues to provide new insight into the history of Mars' water and atmosphere, and offers tantalizing clues at the possibility of past or present Martian lifeforms.</p>
+				<p>Included in <em>Curiosity's</em> payload are 17 cameras, each with a specific purpose ranging from navigation to chemical analysis. The raw data from a number of these cameras is returned in full to Earth. Here at redrovr, we offer a selection of the most fascinating imagery from Gale Crater, continually updated as <em>Curiosity's</em> mission unfolds.</p>
 			</div>
 		</div>
 	</div>
@@ -19,7 +21,7 @@
 				<h3 class="panel-title">About Us</h3>
 			</div>
 			<div class="panel-body">
-				about us
+				<p>The project now known as redrovr began as the creation of four aspiring web developers, taking the dive into the world of code and design. Each brought a unique set of skills and ideas to contribute to the creative process. The results are now displayed here, and we hope that our efforts can make it a little easier to enjoy and appreciate the wonder of exploring the universe.</p>
 			</div>
 		</div>
 	</div>
