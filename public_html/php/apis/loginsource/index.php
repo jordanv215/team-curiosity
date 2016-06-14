9be41470ce45713dec50a61d5e627279f1736ea3
Anthony Williams <mylifeassurance@aol.com>
2016-06-14 11:31:09 -0600

    Changed reddit api

--- public_html/php/apis/loginsource/index.php
+++ public_html/php/apis/loginsource/index.php
@@ -27,8 +27,132 @@ $state = random_int($min, $max);
 
 $redirectUri = 
 
+setcookie("reddit_login_nonce", $state);
+header("Location: " . "https://www.reddit.com/api/v1/authorize.compact?client_id=". $clientId ."&response_type=code&state=". $state ."&redirect_uri=". $redirectUri ."&duration=3600&scope=identity");
+// commenting in api finds
+$config = readConfig("/etc/apache2/capstone-mysql/mars.ini");
+$keys = $config["authkeys"];
+$clientId = $keys->reddit->clientId;
 
-"https://www.reddit.com/api/v1/authorize.compact?client_id=$&response_type=code&state=$&redirect_uri=$&duration=3600&scope=identity";
+
+$clientSecret = $keys->reddit->clientSecret;
+
+$username = "redrovr";
+
+if(isset($_GET["error"]))
+{
+	//error occured
+	echo $_GET["error"];
+}
+else
+{
+	//validate nonce
+	if($_COOKIE["reddit_login_nonce"] == $_GET["state"])
+	{
+		//valid nonce
+
+		//now make a post request with one time code to generate access token
+		$url = "https://www.reddit.com/api/v1/access_token";
+		$fields = array("grant_type" => "authorization_code", "code" => $_GET["code"], "redirect_uri" => "http://redrovr.io");
+
+		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		rtrim($fields_string, '&');
+
+		$ch = curl_init();
+
+		curl_setopt($ch,CURLOPT_URL, $url);
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, $client_id.":".$client_secret);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+
+		curl_close($ch);
+
+		//this is the access token which will used to retrieve user information. You can store this token in database and use it in future.
+		$access_token = $result->access_token;
+	}
+	else
+	{
+		//invalid nonce
+		echo "Please try to login again";
+	}
+}
+// this is the next section of the found api data
+function user_identity($access_token)
+{
+	//lets retrieve username of the logged in user
+	$user_info_url = "https://oauth.reddit.com/api/v1/me";
+	$username = "redrovr";
+
+	$ch = curl_init();
+	curl_setopt($ch,CURLOPT_URL,$user_info_url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: bearer ".$access_token, "User-Agent: redrovr/1.0 by ".$username));
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+
+	$result = curl_exec($ch);
+	$result = json_decode($result);
+
+	if(isset($result->error))
+	{
+		//access token has expired. Use the refresh token to get a new access token and then make REST api calls.
+		$url = "https://ssl.reddit.com/api/v1/access_token";
+		$fields = array("grant_type" => "refresh_token", "refresh_token" => $refresh_token);
+
+		foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+		rtrim($fields_string, '&');
+
+		$ch = curl_init();
+
+		curl_setopt($ch,CURLOPT_URL, "https://ssl.reddit.com/api/v1/access_token");
+		curl_setopt($ch,CURLOPT_POST, count($fields));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+		curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		curl_setopt($ch, CURLOPT_USERPWD, $client_id.":".$client_secret);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+		$result = curl_exec($ch);
+		$result = json_decode($result);
+
+		//new access token
+		$access_token = $result->access_token;
+
+		curl_close($ch);
+
+		$user_info_url = "https://oauth.reddit.com/api/v1/me";
+
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL,$user_info_url);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array("Authorization: bearer ".$access_token, "User-Agent: flairbot/1.0 by ".$username));
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+
+
+		$result = curl_exec($ch);
+		curl_close($ch);
+
+		$result = json_decode($result);
+		echo $user_name = $result->name;
+	}
+	else
+	{
+		echo $user_name = $result->name;
+	}
+
+	curl_close($ch);
+}
+//this is the end of the copied code for this section
+
+
+
+
+
+
 // create an empty reply
 $reply = new stdClass();
 $reply->status = 200;
@@ -141,4 +265,6 @@ try {
 
 // encode and return the reply to the frontend caller
 echo json_encode($reply);
+
+
 
