dfd31f95049895118590799bb119f36968cefe67
Jordan Vinson <jvinson3@cnm.edu>
2016-06-15 09:04:18 -0600

    Swipebox implemented and then deleted

--- public_html/php/templates/head-utils.php
+++ public_html/php/templates/head-utils.php
@@ -44,6 +44,7 @@
 		<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/angularjs/<?php echo $ANGULAR_VERSION;?>/angular-route.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/<?php echo $ANGULAR_VERSION;?>/angular-animate.min.js"></script>
 		<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/1.3.3/ui-bootstrap-tpls.min.js"></script>
+		
 
 		<!--loading Angular app files -->
 		<script src="angular/redrovr.js"></script>
