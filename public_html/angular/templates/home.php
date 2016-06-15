8f4ccfac1fd13babe3bd26b1248d2c05ee68055e
Kai Garrott <garrottkai@gmail.com>
2016-06-15 01:00:48 -0600

    cosmetic fixes

--- public_html/angular/templates/home.php
+++ public_html/angular/templates/home.php
@@ -38,7 +38,7 @@
 	<!-----------/// Carousels for home page ///------------->
 <div class="row carousel-row">
 
-<!--// Image carousel: left on desktop //-->
+<!------// Image carousel: left on desktop //------->
 
 <div class="col-md-6">
 	<div id="imageCarouselSplash" class="carousel slide" data-ride="carousel">
@@ -92,6 +92,7 @@
 		</a>
 	</div>
 </div>
+	<!--------// News carousel: right on desktop //-------------->
 	<!-- Wrapper for slides -->
 
 	<div class="col-md-6">
@@ -107,6 +108,7 @@
 					<div class="carousel-header">
 						<h3 class="news-header">Second Cycle of Martian Seasons Completing for Curiosity Rover</h3>
 						</div>
+						<p class="news-content">May 11, 2016</p>
 						<p class="news-content">NASA's Curiosity Mars rover has completed its second Martian year since landing in 2012, recording environmental patterns through two full cycles of Martian seasons.</p>
 						<div class="news-content">
 							<a href="http://mars.jpl.nasa.gov/msl/news/whatsnew/index.cfm?FuseAction=ShowNews&NewsID=1908" target="_blank">Read more&hellip;</a>
@@ -116,6 +118,7 @@
 				<div class="item">
 					<div class="carousel-header">
 						<h3 class="news-header">Curiosity Mars Rover Crosses Rugged Plateau</h3>
+						<p class="news-content">April 27, 2016</p>
 						<p class="news-content">NASA's Curiosity Mars rover has nearly finished crossing a stretch of the most rugged and difficult-to-navigate terrain encountered during the mission's 44 months on Mars.</p>
 						<div class="news-content">
 							<a<a href="http://mars.jpl.nasa.gov/msl/news/whatsnew/index.cfm?FuseAction=ShowNews&NewsID=1908" target="_blank">Read more&hellip;</a>
