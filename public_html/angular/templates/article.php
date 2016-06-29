53595ce859d01174eac3f170d4452e748bf0cf1c
Ellen Liu <huailun@hotmail.com>
2016-06-28 23:29:05 -0600

    edited facebook login https://app.asana.com/0/117435667927136/118836678397155

--- public_html/angular/templates/article.php
+++ public_html/angular/templates/article.php
@@ -43,24 +43,97 @@
 			</div>
 
 			<div class="modal-body">
-				<div class="form-group">
-					<input type="text" id="username" placeholder="Enter your email or phone" value="" class="form-control login-field">
-					<i class="fa fa-envelope-o login-field-icon"></i>
-				</div>
+				<script>
+					// This is called with the results from from FB.getLoginStatus().
+					function statusChangeCallback(response) {
+						console.log('statusChangeCallback');
+						console.log(response);
+						// The response object is returned with a status field that lets the
+						// app know the current login status of the person.
+						// Full docs on the response object can be found in the documentation
+						// for FB.getLoginStatus().
+						if (response.status === 'connected') {
+							// Logged into your app and Facebook.
+							testAPI();
+						} else if (response.status === 'not_authorized') {
+							// The person is logged into Facebook, but not your app.
+							document.getElementById('status').innerHTML = 'Please log ' +
+								'into this app.';
+						} else {
+							// The person is not logged into Facebook, so we're not sure if
+							// they are logged into this app or not.
+							document.getElementById('status').innerHTML = 'Please log ' +
+								'into Facebook.';
+						}
+					}
 
-				<div class="form-grouop">
-					<input type="text" id="login-pass" placeholder="Password" value="" class="form-control login-field">
-					<i class="fa fa-lock login-field-icon"></i>
-				</div>
+					// This function is called when someone finishes with the Login
+					// Button.  See the onlogin handler attached to it in the sample
+					// code below.
+					function checkLoginState() {
+						FB.getLoginStatus(function(response) {
+							statusChangeCallback(response);
+						});
+					}
 
-				<a href="#" class="btn btn-success modal-login-btn">Log in</a>
-				<a href="#" class="login-link text-center">Forgot your password?</a>
-			</div>
-			<div class="text-center">OR</div>
+					window.fbAsyncInit = function() {
+						FB.init({
+							appId      : '1701694373430554',
+							cookie     : true,  // enable cookies to allow the server to access
+													  // the session
+							xfbml      : true,  // parse social plugins on this page
+							version    : 'v2.5' // use graph api version 2.5
+						});
 
-			<div class="form-group modal-register-btn text-center">
-				<button class="btn btn-default">Sign Up using Facebook</button>
-			</div>
+						// Now that we've initialized the JavaScript SDK, we call
+						// FB.getLoginStatus().  This function gets the state of the
+						// person visiting this page and can return one of three states to
+						// the callback you provide.  They can be:
+						//
+						// 1. Logged into your app ('connected')
+						// 2. Logged into Facebook, but not your app ('not_authorized')
+						// 3. Not logged into Facebook and can't tell if they are logged into
+						//    your app or not.
+						//
+						// These three cases are handled in the callback function.
+
+						FB.getLoginStatus(function(response) {
+							statusChangeCallback(response);
+						});
+
+					};
+
+					// Load the SDK asynchronously
+					(function(d, s, id) {
+						var js, fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) return;
+						js = d.createElement(s); js.id = id;
+						js.src = "//connect.facebook.net/en_US/sdk.js";
+						fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));
+
+					// Here we run a very simple test of the Graph API after login is
+					// successful.  See statusChangeCallback() for when this call is made.
+					function testAPI() {
+						console.log('Welcome!  Fetching your information.... ');
+						FB.api('/me', function(response) {
+							console.log('Successful login for: ' + response.name);
+							document.getElementById('status').innerHTML =
+								'Thanks for logging in, ' + response.name + '!';
+						});
+					}
+				</script>
+				<div id="fb-root"></div>
+				<script>(function(d, s, id) {
+						var js, fjs = d.getElementsByTagName(s)[0];
+						if (d.getElementById(id)) return;
+						js = d.createElement(s); js.id = id;
+						js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6";
+						fjs.parentNode.insertBefore(js, fjs);
+					}(document, 'script', 'facebook-jssdk'));</script>
+
+				<div class="fb-login-button" data-max-rows="1" data-size="medium" data-show-faces="false" data-auto-logout-link="false"></div>
+
 			<div class="modal-footer">
 				<button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
 			</div>
